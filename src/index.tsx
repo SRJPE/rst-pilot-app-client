beb67a7dffd1aafc91ddaf7dec5881e66a0d5397
Ben Pintel <Bpintel@gmail.com>
2022-09-07 17:09:43 -0700

    Refactors Root Stack Navigation

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,22 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { RootStackParamList } from './types'
 import Home from './components/Home/Home'
-import DrawerNavigator from './components/TrapVisitForm/DrawerNavigator'
 import TrapVisitForm from './components/TrapVisitForm/FormContainer/TrapVisitForm'
+import GenerateReport from './components/GenerateReport/GenerateReport'
+import DataQualityControl from './components/QCData/DataQualityControl'
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator<RootStackParamList>()
 
 export default function Navigator() {
   return (
     <Stack.Navigator initialRouteName='Home'>
       <Stack.Screen name='Home' component={Home} />
-      <Stack.Screen name='Trap Visit Data Entry' component={TrapVisitForm} />
-
-      <Stack.Screen name='DrawerNavigator' component={DrawerNavigator} />
+      <Stack.Screen name='Generate Report' component={GenerateReport} />
+      <Stack.Screen name='Trap Visit Form' component={TrapVisitForm} />
+      <Stack.Screen
+        name='Data Quality Control'
+        component={DataQualityControl}
+      />
     </Stack.Navigator>
   )
 }
