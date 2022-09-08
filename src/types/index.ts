37ae1081726c12eb0ba68b99429327b2fdee3083
Ben Pintel <Bpintel@gmail.com>
2022-09-07 17:16:05 -0700

    Adds folders and index files for: interfaces types utils

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export type RootStackParamList = {
+  Home: undefined
+  'Generate Report': undefined
+  'Trap Visit Form': undefined
+  'Data Quality Control': undefined
+}
+
+export type FormTabParamList = {
+  'Visit Setup': undefined
+  'Trap Status': undefined
+  'Fish Input': undefined
+  'Fish Processing': undefined
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,11 @@
+export const getInitials = (name: string) => {
+  let rgx = new RegExp(/(\p{L}{1})\p{L}+/, 'gu')
+
+  let initials = [...name.matchAll(rgx)] || []
+
+  const result = (
+    (initials.shift()?.[1] || '') + (initials.pop()?.[1] || '')
+  ).toUpperCase()
+
+  return result
+}
