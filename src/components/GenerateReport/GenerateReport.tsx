5478a6e10a65b2c5226ce7b4f08288cc6c16456f
Ben Pintel <Bpintel@gmail.com>
2022-09-07 17:08:28 -0700

    Adds placeholder components

--- src/components/GenerateReport/GenerateReport.tsx
+++ src/components/GenerateReport/GenerateReport.tsx
@@ -1,6 +1,10 @@
-import { Text } from 'native-base'
+import { Text, View } from 'native-base'
 import React from 'react'
 
 export default function GenerateReport() {
-  return <Text>GenerateReport</Text>
+  return (
+    <View flex={1} justifyContent='center' alignItems='center'>
+      <Text fontSize='lg'>Generate Report Placeholder</Text>
+    </View>
+  )
 }
--- src/components/QCData/DataQualityControl.tsx
+++ src/components/QCData/DataQualityControl.tsx
@@ -1,6 +1,10 @@
-import { Text } from 'native-base'
+import { Text, View } from 'native-base'
 import React from 'react'
 
 export default function DataQualityControl() {
-  return <Text>DataQualityControl</Text>
+  return (
+    <View flex={1} justifyContent='center' alignItems='center'>
+      <Text fontSize='lg'>Data Quality Control Placeholder</Text>
+    </View>
+  )
 }
