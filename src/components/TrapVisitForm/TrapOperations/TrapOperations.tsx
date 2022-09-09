7f0598d48fa577f3e86c8a840a97b85f0a49597a
Ben Pintel <Bpintel@gmail.com>
2022-09-08 20:24:57 -0700

    Adds to outline of Trap Operations

--- src/components/TrapVisitForm/TrapOperations/TrapOperations.tsx
+++ src/components/TrapVisitForm/TrapOperations/TrapOperations.tsx
@@ -19,11 +19,12 @@ export default function TrapOperations() {
   const [coneDepth, setConeDepth] = useState('' as string)
   const [coneSetting, setConeSetting] = useState('full' as string)
   const [totalRevolutions, setTotalRevolutions] = useState('' as string)
+  const [checked, setChecked] = useState(false as boolean)
 
   return (
-    <Box height='full' bg='#fff' padding='15'>
-      <VStack space={4}>
-        <Heading fontSize='lg'>Trap Pre-Processing</Heading>
+    <Box height='full' bg='#fff' p='50'>
+      <VStack space={8}>
+        <Heading>Trap Pre-Processing</Heading>
         <FormControl w='1/4'>
           <FormControl.Label>Cone Depth</FormControl.Label>
           <Input
@@ -51,7 +52,7 @@ export default function TrapOperations() {
             </Radio>
           </Radio.Group>
         </FormControl>
-        <HStack space={6} alignItems='flex-start'>
+        <HStack space={6} alignItems='center'>
           <FormControl w='1/4'>
             <FormControl.Label>Total Revolutions</FormControl.Label>
             <Input
@@ -61,14 +62,60 @@ export default function TrapOperations() {
               keyboardType='numeric'
             />
           </FormControl>
-          <FormControl w='1/4'>
-            <FormControl.Label>Collect after fish processing</FormControl.Label>
-            <Checkbox
-              value='test'
-              accessibilityLabel='Collect after fish processing'
-            />
+          <FormControl w='1/2'>
+            <HStack space={4} alignItems='center' pt='6'>
+              <Checkbox
+                isChecked={checked}
+                onChange={() => setChecked(!checked)}
+                colorScheme='primary'
+                value=''
+                accessibilityLabel='Collect total revolutions after fish processing Checkbox'
+              />
+              <FormControl.Label>
+                Collect total revolutions after fish processing
+              </FormControl.Label>
+            </HStack>
           </FormControl>
         </HStack>
+        <FormControl w='3/4'>
+          <FormControl.Label>RPM Before Cleaning</FormControl.Label>
+          <HStack space={4} alignItems='center'>
+            <FormControl w='1/4'>
+              <FormControl.Label>Measurement 1</FormControl.Label>
+              <Input
+                onChangeText={setTotalRevolutions}
+                value={totalRevolutions}
+                placeholder='Numeric Value'
+                keyboardType='numeric'
+              ></Input>
+            </FormControl>
+            <FormControl w='1/4'>
+              <FormControl.Label>Measurement 2</FormControl.Label>
+              <Input
+                onChangeText={setTotalRevolutions}
+                value={totalRevolutions}
+                placeholder='Numeric Value'
+                keyboardType='numeric'
+              ></Input>
+            </FormControl>
+            <FormControl w='1/4'>
+              <FormControl.Label>Measurement 3</FormControl.Label>
+              <Input
+                onChangeText={setTotalRevolutions}
+                value={totalRevolutions}
+                placeholder='Numeric Value'
+                keyboardType='numeric'
+              ></Input>
+            </FormControl>
+          </HStack>
+        </FormControl>
+        <Text color='grey'>
+          Please take 3 separate measures of cone rotations per minute before
+          cleaning the trap.
+        </Text>
+        <Heading alignSelf='center' fontSize='lg'>
+          Remove debris and begin fish processing
+        </Heading>
       </VStack>
     </Box>
   )
