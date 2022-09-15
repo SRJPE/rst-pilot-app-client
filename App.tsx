3720ef1b645e048497f260d43b40d2eb9721f12c
Ben Pintel <Bpintel@gmail.com>
2022-09-15 12:49:52 -0700

    Renames DrawerNavigation to Main DrawerNavigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import 'react-native-gesture-handler'
 import AppContainer from './src/'
-import DrawerNavigator from './src/navigators/DrawerNavigator'
+import MainDrawerNavigator from './src/navigators/MainDrawerNavigator'
 
 export default function App() {
   return (
     <AppContainer>
-      <DrawerNavigator />
+      <MainDrawerNavigator />
     </AppContainer>
   )
 }
