2476fd9c6ddd93712c6cacc7b14410520ca3c41b
Hunter H <hunter.h.dev@gmail.com>
2022-09-20 14:08:42 -0700

    update styling on home screen

--- src/components/home/HomeNavButtons.tsx
+++ src/components/home/HomeNavButtons.tsx
@@ -1,5 +1,37 @@
 import React, { useCallback } from 'react'
-import { Button, HStack, Flex } from 'native-base'
+import { Button, HStack, Flex, CircleIcon } from 'native-base'
+import { StyleSheet, Text, View } from 'react-native'
+import { Entypo } from '@expo/vector-icons'
+import { MaterialIcons } from '@expo/vector-icons'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+const styles = StyleSheet.create({
+  button: {
+    flex: 1,
+    height: 150,
+    borderRadius: 0,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 15,
+    marginTop: 10,
+  },
+  centeredColumn: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonBackground: {
+    backgroundColor: '#263238',
+    width: 75,
+    height: 75,
+    borderRadius: 100,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+})
 
 export default function BottomNavigation({ navigation }: { navigation: any }) {
   const handlePressQCData = useCallback(() => {
@@ -14,30 +46,49 @@ export default function BottomNavigation({ navigation }: { navigation: any }) {
 
   return (
     <Flex align='center'>
-      <HStack space={1}>
+      <HStack space={0.5}>
         <Button
-          w='33%'
           variant='solid'
           bg='primary'
           onPress={handlePressQCData}
+          style={styles.button}
         >
-          QC Data
+          <View style={styles.centeredColumn}>
+            <View style={styles.buttonBackground}>
+              <MaterialIcons name='timeline' size={50} color='white' />
+            </View>
+            <Text style={styles.buttonText}>QC Data</Text>
+          </View>
         </Button>
         <Button
-          w='33%'
           variant='solid'
           bg='primary'
           onPress={handlePressTrapVisit}
+          style={styles.button}
         >
-          Collect Data
+          <View style={styles.centeredColumn}>
+            <View style={styles.buttonBackground}>
+              <Entypo name='plus' size={50} color='white' />
+            </View>
+            <Text style={styles.buttonText}>Collect Data</Text>
+          </View>
         </Button>
         <Button
-          w='33%'
           variant='solid'
           bg='primary'
           onPress={handlePressGenerateReport}
+          style={styles.button}
         >
-          Generate Report
+          <View style={styles.centeredColumn}>
+            <View style={styles.buttonBackground}>
+              <MaterialCommunityIcons
+                name='clipboard-text'
+                size={50}
+                color='white'
+              />
+            </View>
+            <Text style={styles.buttonText}>Generate Report</Text>
+          </View>
         </Button>
       </HStack>
     </Flex>
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -6,31 +6,89 @@ import TopIcons from '../../components/home/TopIcons'
 import { View, StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-  contentContainer: {
+  landingContent: {
     width: '100%',
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center'
+    alignItems: 'center',
+  },
+  recentItemsContainer: {
+    minHeight: 75,
+    alignSelf: 'flex-end',
+    width: '100%',
+    paddingHorizontal: 50,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'stretch',
+  },
+  recentItemsCardRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  recentItemsCard: {
+    height: 200,
+    width: 200,
+    borderWidth: 1,
+    borderColor: '#A29C9C',
+    borderRadius: 5,
+    display: 'flex',
+  },
+  recentItemsCardPreviewContainer: {
+    backgroundColor: '#D9D9D9',
+    flex: 3,
+  },
+  recentItemsCardTextContainer: {
+    flex: 2,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    display: 'flex',
+    justifyContent: 'space-between',
   },
 })
 
 export default function Home({ navigation }: { navigation: any }) {
+  const recentItemsCard = ({
+    title,
+    date,
+  }: {
+    title: string
+    date: string
+  }) => {
+    return (
+      <View style={styles.recentItemsCard}>
+        <View style={styles.recentItemsCardPreviewContainer}></View>
+        <View style={styles.recentItemsCardTextContainer}>
+          <Text fontSize={19} maxWidth={150}>{title}</Text>
+          <Text color='#A1A1A1'>{date}</Text>
+        </View>
+      </View>
+    )
+  }
+
   return (
-    <VStack
-      height='full'
-      space={100}
-      alignItems='center'
-      justifyContent='space-between'
-    >
+    <VStack height='full' alignItems='center' justifyContent='space-between'>
       <TopIcons />
 
-      <View style={styles.contentContainer}>
+      <View style={styles.landingContent}>
         <CircleIcon size='300' color='primary' />
-        <Heading fontWeight={300}>Welcome back, Jordan!</Heading>
-        <Text>Select the action you would like to preform.</Text>
-        <Center bg='secondary' h='100' w='90%'>
-          <Text>Recent Items Placeholder</Text>
-        </Center>
+        <Heading fontWeight={300} fontSize={50} marginTop={5}>
+          Welcome back, Jordan!
+        </Heading>
+        <Text fontWeight={300} fontSize={23} marginTop={5}>
+          Select the action you would like to preform.
+        </Text>
+      </View>
+
+      <View style={styles.recentItemsContainer}>
+        <Text fontWeight={300} fontSize={20} marginBottom={5}>
+          Recent Items
+        </Text>
+        <View style={styles.recentItemsCardRow}>
+          {recentItemsCard({ title: 'Trap Visit Data Entry', date: '7/21/22' })}
+          {recentItemsCard({ title: 'Report', date: '7/22/22' })}
+          {recentItemsCard({ title: 'Quality Control', date: '7/24/22' })}
+        </View>
       </View>
 
       <BottomNavigation navigation={navigation} />
