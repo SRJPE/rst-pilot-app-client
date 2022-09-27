99e596e7b5c95124aca97665b0f9d07fbe80397c
Ben Pintel <Bpintel@gmail.com>
2022-09-27 13:44:05 -0700

    Begin to refactor drawerMenu to support navigation redux  state

--- src/navigators/DrawerMenu.tsx
+++ src/navigators/DrawerMenu.tsx
@@ -5,7 +5,6 @@ import {
   Avatar,
   Heading,
   IconButton,
-  useColorModeValue,
   Box,
   Divider,
 } from 'native-base'
@@ -16,70 +15,54 @@ import {
 } from '@react-navigation/drawer'
 import Ionicons from '@expo/vector-icons/Ionicons'
 import MenuButton from '../components/drawerMenu/MenuButton'
-import ThemeToggle from '../components/drawerMenu/ThemeToggle'
-
-//temporary routing during development into nested form.
+import { useSelector } from 'react-redux'
+import { AppDispatch } from '../redux/store'
+import { useDispatch } from 'react-redux'
+import { updateActiveStep } from '../redux/reducers/navigationSlice'
 
 export default function DrawerMenu(props: DrawerContentComponentProps) {
   const { state, navigation } = props
   const currentRoute = state.routeNames[state.index]
 
-  const handlePressBackButton = useCallback(() => {
-    navigation.closeDrawer()
-  }, [navigation])
-  const handlePressMenuHome = useCallback(() => {
-    navigation.navigate('Home')
-  }, [navigation])
-  const handlePressMenuGenerateReport = useCallback(() => {
-    navigation.navigate('Generate Report')
-  }, [navigation])
-  const handlePressMenuTrapVisitForm = useCallback(() => {
-    navigation.navigate('Trap Visit Form')
-  }, [navigation])
-  const handlePressMenuVisitSetup = useCallback(() => {
-    navigation.navigate('Trap Visit Form', { screen: 'Visit Setup' })
-  }, [navigation])
-  const handlePressMenuTrapStatus = useCallback(() => {
-    navigation.navigate('Trap Visit Form', { screen: 'Trap Status' })
-  }, [navigation])
-  const handlePressMenuTrapOperations = useCallback(() => {
-    navigation.navigate('Trap Visit Form', { screen: 'Trap Operations' })
-  }, [navigation])
-  const handlePressMenuFishInput = useCallback(() => {
-    navigation.navigate('Trap Visit Form', { screen: 'Fish Input' })
-  }, [navigation])
-  const handlePressMenuFishProcessing = useCallback(() => {
-    navigation.navigate('Trap Visit Form', { screen: 'Fish Processing' })
-  }, [navigation])
-  const handlePressMenuHighFlows = useCallback(() => {
-    navigation.navigate('High Flows')
-  }, [navigation])
-  const handlePressMenuHighTemperatures = useCallback(() => {
-    navigation.navigate('High Temperatures')
-  }, [navigation])
-  const handlePressMenuNonFunctionalTrap = useCallback(() => {
-    navigation.navigate('Non Functional Trap')
-  }, [navigation])
-  const handlePressMenuNoFishCaught = useCallback(() => {
-    navigation.navigate('No Fish Caught')
-  }, [navigation])
+  const dispatch = useDispatch<AppDispatch>()
+  const navigationState = useSelector((state: any) => state.navigation)
+  const { steps, activeStep } = navigationState
+  console.log('🚀 ~ DrawerMenu ~ activeStep', activeStep)
+  console.log('🚀 ~ DrawerMenu ~ steps', steps)
+  // const activePageTitle = steps[activeStep].name
+  const stepsArray = Object.values(steps)
+
+  const handlePressMainNavButton = useCallback(
+    (buttonTitle: string) => {
+      navigation.navigate(buttonTitle)
+    },
+    [navigation]
+  )
+
+  const handlePressFormButton = useCallback((buttonTitle: string) => {
+    navigation.navigate('Trap Visit Form', { screen: buttonTitle })
+    dispatch({
+      type: updateActiveStep,
+      payload: navigationState[buttonTitle],
+    })
+  }, [])
 
   return (
-    <Box safeArea flex={1} bg={useColorModeValue('#FFFFFF', 'primary')} p={7}>
+    <Box safeArea flex={1} p={7}>
       <VStack flex={1} space={2}>
         <HStack justifyContent='flex-end'>
           <IconButton
-            onPress={handlePressBackButton}
+            onPress={() => navigation.closeDrawer()}
             borderWidth={2}
             borderRadius={100}
             variant='solid'
-            backgroundColor={useColorModeValue('primary', '#FFFFFF')}
-            borderColor={useColorModeValue('primary', '#FFFFFF')}
+            backgroundColor='primary'
+            borderColor='primary'
             _icon={{
               as: Ionicons,
               name: 'chevron-back',
               size: 6,
-              color: useColorModeValue('#FFFFFF', 'primary'),
+              color: '#FFF',
             }}
           />
         </HStack>
@@ -97,95 +80,39 @@ export default function DrawerMenu(props: DrawerContentComponentProps) {
         <DrawerContentScrollView>
           <MenuButton
             active={currentRoute === 'Home'}
-            onPress={handlePressMenuHome}
+            onPress={() => handlePressMainNavButton('Home')}
             icon='home'
-          >
-            Home
-          </MenuButton>
+            title='Home'
+          />
           <MenuButton
             active={currentRoute === 'Generate Report'}
-            onPress={handlePressMenuGenerateReport}
+            onPress={() => handlePressMainNavButton('Generate Report')}
             icon='podium'
-          >
-            Generate Report
-          </MenuButton>
+            title='Generate Report'
+          />
+
           <MenuButton
             active={currentRoute === 'Trap Visit Form'}
-            onPress={handlePressMenuTrapVisitForm}
+            onPress={() => handlePressMainNavButton('Trap Visit Form')}
             icon='clipboard'
-          >
-            Trap Visit Form
-          </MenuButton>
+            title='Trap Visit Form'
+          />
           <Divider />
-          <VStack ml='8'>
-            <MenuButton
-              active={currentRoute === 'Visit Setup'}
-              onPress={handlePressMenuVisitSetup}
-              icon='clipboard'
-            >
-              Visit Setup
-            </MenuButton>
-            <MenuButton
-              active={currentRoute === 'Trap Status'}
-              onPress={handlePressMenuTrapStatus}
-              icon='clipboard'
-            >
-              Trap Status
-            </MenuButton>
-            <MenuButton
-              active={currentRoute === 'Trap Operations'}
-              onPress={handlePressMenuTrapOperations}
-              icon='clipboard'
-            >
-              Trap Operations
-            </MenuButton>
-            <MenuButton
-              active={currentRoute === 'Fish Input'}
-              onPress={handlePressMenuFishInput}
-              icon='clipboard'
-            >
-              Fish Input
-            </MenuButton>
-            <MenuButton
-              active={currentRoute === 'Fish Processing'}
-              onPress={handlePressMenuFishProcessing}
-              icon='clipboard'
-            >
-              Fish Processing
-            </MenuButton>
-            <MenuButton
-              active={currentRoute === 'High Flows'}
-              onPress={handlePressMenuHighFlows}
-              icon='clipboard'
-            >
-              High Flows
-            </MenuButton>
-            <MenuButton
-              active={currentRoute === 'High Temperatures'}
-              onPress={handlePressMenuHighTemperatures}
-              icon='clipboard'
-            >
-              High Temperatures
-            </MenuButton>
-            <MenuButton
-              active={currentRoute === 'Non Functional Trap'}
-              onPress={handlePressMenuNonFunctionalTrap}
-              icon='clipboard'
-            >
-              Non Functional Trap
-            </MenuButton>
-            <MenuButton
-              active={currentRoute === 'No Fish Caught'}
-              onPress={handlePressMenuNoFishCaught}
-              icon='clipboard'
-            >
-              No Fish Caught
-            </MenuButton>
-          </VStack>
+          {stepsArray &&
+            stepsArray.map((step: any, index: any) => {
+              return (
+                <VStack ml='8' key={index}>
+                  <MenuButton
+                    active={currentRoute === step.name}
+                    onPress={() => handlePressFormButton(step.name)}
+                    icon='clipboard'
+                    title={step.name}
+                    // disabled={step.completed}
+                  />
+                </VStack>
+              )
+            })}
         </DrawerContentScrollView>
-        {/* <Center>
-          <ThemeToggle />
-        </Center> */}
       </VStack>
     </Box>
   )
