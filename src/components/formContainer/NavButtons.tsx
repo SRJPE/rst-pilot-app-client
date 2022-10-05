18bcaa6e938dba1fcc89d08105161705ffee5127
Ben Pintel <Bpintel@gmail.com>
2022-10-05 16:56:56 -0700

    In the process of cleaning up and refactoring

--- src/components/formContainer/NavButtons.tsx
+++ src/components/formContainer/NavButtons.tsx
@@ -24,39 +24,62 @@ export default function NavButtons({
   const activePage = navigationState.steps[activeStep]?.name
   const reduxState = useSelector((state: any) => state)
 
-  const navigateFlow = (values: any) => {
-    if (values?.trapStatus === 'Trap stopped functioning') {
-      navigation.navigate('Trap Visit Form', {
-        screen: 'Non Functional Trap',
-      })
-      dispatch({
-        type: updateActiveStep,
-        payload: 8,
-      })
-    } else if (values?.flowMeasure > 1000) {
-      navigation.navigate('Trap Visit Form', { screen: 'High Flows' })
-      dispatch({
-        type: updateActiveStep,
-        payload: 6,
-      })
-    } else if (values?.waterTemperature > 30) {
-      navigation.navigate('Trap Visit Form', { screen: 'High Temperatures' })
-      dispatch({
-        type: updateActiveStep,
-        payload: 7,
-      })
+  const navigateHelper = (destination: string, payload: number) => {
+    navigation.navigate('Trap Visit Form', { screen: destination })
+    dispatch({
+      type: updateActiveStep,
+      payload: payload,
+    })
+  }
+
+  const navigateFlowRightButton = (values: any) => {
+    switch (activePage) {
+      case 'Trap Status':
+        if (values?.trapStatus === 'Trap stopped functioning') {
+          navigateHelper('Non Functional Trap', 8)
+        } else if (values?.flowMeasure > 1000) {
+          navigateHelper('High Flows', 6)
+        } else if (values?.waterTemperature > 30) {
+          navigateHelper('High Temperatures', 7)
+        }
+        break
+      case 'Fish Processing':
+        if (values?.fishProcessed === 'No fish were caught') {
+          console.log('inside')
+          navigateHelper('No Fish Caught', 9)
+        }
+        break
+      case 'High Flows':
+        navigateHelper('End Trapping', 10)
+        break
+      case 'High Temperatures':
+        navigateHelper('Fish Processing', 4)
+        break
+      default:
+        console.log('default')
+        break
+    }
+  }
+
+  const navigateFlowLeftButton = () => {
+    if ([6, 7, 8].includes(activeStep)) {
+      //return to trap status flow if on warning screen
+      navigateHelper('Trap Status', 2)
+    } else if (activeStep === 9) {
+      //return to Fish Processing if on no fish caught screen
+      navigateHelper('Fish Processing', 4)
     }
   }
 
   const handleRightButton = () => {
-    // console.log('🚀 ~ handleRightButton ~ values', values)
-    if (activeStep === 5) return
-    navigateFlow(values)
+    //if function truthy, submit form to check for errors and save to redux
+    if (handleSubmit) {
+      handleSubmit()
+    }
 
-    // console.log('🚀 ~ touched form Form', touched)
-    // console.log('🚀 ~ errors from Form', errors)
-    //submit form to check for errors
-    handleSubmit()
+    //navigate various flows
+    navigateFlowRightButton(values)
+
     //if form has not been touched OR there are errors => return out, otherwise navigate
     if (Object.keys(touched).length === 0 || Object.keys(errors).length > 0) {
       return
@@ -72,16 +95,25 @@ export default function NavButtons({
   }
 
   const handleLeftButton = () => {
-    // console.log('🚀 ~ handleLeftButton ~ validation', validation)
-    handleSubmit()
-
-    navigation.navigate('Trap Visit Form', {
-      screen: navigationState.steps[activeStep - 1]?.name,
-    })
-    dispatch({
-      type: updateActiveStep,
-      payload: navigationState.activeStep - 1,
-    })
+    //if function truthy, submit form to save to redux
+    if (handleSubmit) {
+      handleSubmit()
+    }
+    if ([6, 7, 8].includes(activeStep)) {
+      //return to trap status flow if on a warning screen
+      navigateHelper('Trap Status', 2)
+    } else if (activeStep === 9) {
+      //return to Fish Processing if on no fish caught screen
+      navigateHelper('Fish Processing', 4)
+    } else {
+      navigation.navigate('Trap Visit Form', {
+        screen: navigationState.steps[activeStep - 1]?.name,
+      })
+      dispatch({
+        type: updateActiveStep,
+        payload: navigationState.activeStep - 1,
+      })
+    }
   }
 
   const renderButtonText = (activePage: string) => {
@@ -96,15 +128,6 @@ export default function NavButtons({
     return buttonText
   }
 
-  const isDisabled = (activePage: string) => {
-    return activePage === 'Visit Setup' ||
-      activePage === 'High Flows' ||
-      activePage === 'High Temperatures' ||
-      activePage === 'Non Functional Trap'
-      ? true
-      : false
-  }
-
   return (
     <Box bg='themeGrey' py='5' px='3' maxWidth='100%'>
       <HStack justifyContent='space-between'>
@@ -115,7 +138,7 @@ export default function NavButtons({
           py='3'
           width='30%'
           borderRadius='5'
-          isDisabled={isDisabled(activePage)}
+          isDisabled={activePage === 'Visit Setup'}
           onPress={handleLeftButton}
         >
           <Text fontSize='sm' fontWeight='bold' color='primary'>
@@ -142,6 +165,7 @@ export default function NavButtons({
           py='3'
           width='30%'
           borderRadius='5'
+          // isDisabled={}
           onPress={handleRightButton}
         >
           <Text fontSize='sm' fontWeight='bold' color='white'>
@@ -152,3 +176,27 @@ export default function NavButtons({
     </Box>
   )
 }
+
+// //trap status  flow
+// if (values?.trapStatus === 'Trap stopped functioning') {
+//   navigateHelper('Non Functional Trap', 8)
+// } else if (values?.flowMeasure > 1000) {
+//   navigateHelper('High Flows', 6)
+// } else if (values?.waterTemperature > 30) {
+//   navigateHelper('High Temperatures', 7)
+// }
+// //fish processing flow
+// else if (values?.fishProcessed === 'No fish were caught') {
+//   console.log('inside')
+//   navigateHelper('No Fish Caught', 9)
+// }
+
+// //if on Hgh Temp warning page, continue to fish processing
+// else if (activeStep === 7) {
+//   navigateHelper('Fish Processing', 4)
+// }
+// //if on High Flow warning page, continue to end trapping
+// else if (activeStep === 6) {
+//   navigateHelper('End Trapping', 10)
+// }
+// console.log('🚀 ~ navigateFlowRightButton ~ fishProcessed', values)
