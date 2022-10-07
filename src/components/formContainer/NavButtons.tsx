7a615e107dc9da8247275caef4a1795aa0cb3158
Ben Pintel <Bpintel@gmail.com>
2022-10-07 09:48:06 -0700

    adds home icon and disable right button function

--- src/components/formContainer/NavButtons.tsx
+++ src/components/formContainer/NavButtons.tsx
@@ -1,8 +1,9 @@
-import { Box, HStack, Text, Button } from 'native-base'
+import { Box, HStack, Text, Button, Icon } from 'native-base'
 import { useSelector, useDispatch } from 'react-redux'
 import { AppDispatch } from '../../redux/store'
 import { updateActiveStep } from '../../redux/reducers/navigationSlice'
 import { useEffect } from 'react'
+import { Ionicons } from '@expo/vector-icons'
 
 export default function NavButtons({
   navigation,
@@ -56,13 +57,16 @@ export default function NavButtons({
         navigateHelper('Fish Processing', 4)
         break
       default:
+        console.log('default')
         break
     }
   }
 
-
   const navigateFlowLeftButton = () => {
     switch (activePage) {
+      case 'Home':
+        navigation.navigate('Home')
+        break
       case 'High Flows':
         navigateHelper('Trap Status', 2)
         break
@@ -76,36 +80,44 @@ export default function NavButtons({
         navigateHelper('Fish Processing', 4)
         break
       default:
+        console.log('default')
         break
     }
   }
 
-
   const handleRightButton = () => {
     //if function truthy, submit form to check for errors and save to redux
     if (handleSubmit) {
+      console.log('🚀 ~ handleRightButton ~ handleSubmit')
+
       handleSubmit()
     }
     //if form has not been touched OR there are errors => return out, otherwise navigate
-    if (
-      (touched && Object.keys(touched).length === 0) ||
-      (errors && Object.keys(errors).length > 0)
-    ) {
-      return
-    } else {
-      navigation.navigate('Trap Visit Form', {
-        screen: navigationState.steps[activeStep + 1]?.name,
-      })
-      dispatch({
-        type: updateActiveStep,
-        payload: navigationState.activeStep + 1,
-      })
-      //navigate various flows
-      navigateFlowRightButton(values)
-    }
+    // if (
+    //   ((touched && Object.keys(touched).length === 0) ||
+    //     (errors && Object.keys(errors).length > 0)) &&
+    //   activePage !== 'Fish Processing'
+    // ) {
+    //   return
+    // } else {
+    //navigate various flows
+    navigateFlowRightButton(values)
+    navigation.navigate('Trap Visit Form', {
+      screen: navigationState.steps[activeStep + 1]?.name,
+    })
+    dispatch({
+      type: updateActiveStep,
+      payload: navigationState.activeStep + 1,
+    })
+    // }
   }
 
   const handleLeftButton = () => {
+    // if (activePage === ' Visit Setup') {
+    //   navigation.navigate('Home')
+    //   return
+    // }
+
     //if function truthy, submit form to save to redux
     if (handleSubmit) {
       handleSubmit()
@@ -122,7 +134,14 @@ export default function NavButtons({
     navigateFlowLeftButton()
   }
 
-  const renderButtonText = (activePage: string) => {
+  const disableRightButton = () => {
+    return (
+      (touched && Object.keys(touched).length === 0) ||
+      (errors && Object.keys(errors).length > 0)
+    )
+  }
+
+  const renderRightButtonText = (activePage: string) => {
     let buttonText
     switch (activePage) {
       case 'High Flows':
@@ -151,11 +170,14 @@ export default function NavButtons({
           width='1/3'
           borderRadius='5'
           shadow='5'
-          isDisabled={activePage === 'Visit Setup'}
+          // isDisabled={activePage === 'Visit Setup'}
+          leftIcon={
+            <Icon as={Ionicons} name='home' size='sm' color='primary' />
+          }
           onPress={handleLeftButton}
         >
           <Text fontSize='sm' fontWeight='bold' color='primary'>
-            Back
+            {activePage === 'Visit Setup' ? 'Return Home' : 'Back'}
           </Text>
         </Button>
         <Button
@@ -179,11 +201,11 @@ export default function NavButtons({
           width='1/3'
           borderRadius='5'
           shadow='5'
-          // isDisabled={}
+          isDisabled={disableRightButton()}
           onPress={handleRightButton}
         >
           <Text fontSize='sm' fontWeight='bold' color='white'>
-            {renderButtonText(activePage)}
+            {renderRightButtonText(activePage)}
           </Text>
         </Button>
       </HStack>
