b0774000b310318244ca86482bc13509996604f2
Ben Pintel <Bpintel@gmail.com>
2022-10-13 10:37:28 -0700

    Renders Trap Operation as title... ...for Trap Pre & Post Processing screens

--- src/components/formContainer/ProgressHeader.tsx
+++ src/components/formContainer/ProgressHeader.tsx
@@ -1,16 +1,25 @@
 import { Box, HStack, VStack, Text, Progress } from 'native-base'
 import { useSelector } from 'react-redux'
+import TrapPostProcessing from '../../screens/formScreens/trapOperations/TrapPostProcessing'
 
 export default function ProgressHeader(props: any) {
   const navigationState = useSelector((state: any) => state.navigation)
   const { steps, activeStep } = navigationState
   const activePageTitle = steps[activeStep]?.name
 
+  const renderPageTitle = () => {
+    let title
+    activePageTitle === 'Trap Post-Processing' ||
+    activePageTitle === 'Trap Pre-Processing'
+      ? (title = 'Trap Operations')
+      : (title = activePageTitle)
+    return title
+  }
   return (
     <Box>
       <VStack>
         <HStack w='100%' justifyContent='space-between' p='4'>
-          <Text fontSize='2xl'>{activePageTitle || ''}</Text>
+          <Text fontSize='2xl'>{renderPageTitle()}</Text>
           <Text fontSize='xl'>{`Step ${navigationState.activeStep} of ${
             Object.keys(navigationState.steps).length
           }`}</Text>
