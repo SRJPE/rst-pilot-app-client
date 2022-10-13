d10988602823c0181f62a42e5f442f60e923f587
Ben Pintel <Bpintel@gmail.com>
2022-10-13 12:47:37 -0700

    removes nav button import

--- src/screens/trapVisitForm/index.tsx
+++ src/screens/trapVisitForm/index.tsx
@@ -1,5 +1,4 @@
 import { VStack } from 'native-base'
-import NavButtons from '../../components/formContainer/NavButtons'
 import FormStackNavigation from '../../navigators/FormStackNavigation'
 
 export default function TrapVisitForm({ navigation }: { navigation: any }) {
