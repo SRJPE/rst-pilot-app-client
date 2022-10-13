85f19295f356674ab07ccef5ed1fbd386fb35ca7
Ben Pintel <Bpintel@gmail.com>
2022-10-13 10:25:21 -0700

    Adjusts form step numbers

--- src/redux/reducers/navigationSlice.ts
+++ src/redux/reducers/navigationSlice.ts
@@ -3,15 +3,24 @@ import { createSlice } from '@reduxjs/toolkit'
 const formSteps = {
   1: { name: 'Visit Setup', propName: 'visitSetup', completed: false },
   2: { name: 'Trap Status', propName: 'trapStatus', completed: false },
-  3: { name: 'Trap Operations', propName: 'trapOperations', completed: false },
+  3: {
+    name: 'Trap Pre-Processing',
+    propName: 'trapPreProcessing',
+    completed: false,
+  },
   4: { name: 'Fish Processing', propName: 'fishProcessing', completed: false },
   5: { name: 'Fish Input', propName: 'fishInput', completed: false },
-  6: { name: 'High Flows', propName: 'highFlows', completed: false },
-  7: { name: 'High Temperatures', propName: 'highTemperatures' },
-  8: { name: 'Non Functional Trap', propName: 'nonFunctionalTrap' },
-  9: { name: 'No Fish Caught', propName: 'noFishCaught' },
-  10: { name: 'End Trapping', propName: 'endTrapping' },
-  11: { name: 'Incomplete Sections', propName: 'incompleteSections' },
+  6: {
+    name: 'Trap Post-Processing',
+    propName: 'trapPostProcessing',
+    completed: false,
+  },
+  7: { name: 'High Flows', propName: 'highFlows', completed: false },
+  8: { name: 'High Temperatures', propName: 'highTemperatures' },
+  9: { name: 'Non Functional Trap', propName: 'nonFunctionalTrap' },
+  10: { name: 'No Fish Caught', propName: 'noFishCaught' },
+  11: { name: 'End Trapping', propName: 'endTrapping' },
+  12: { name: 'Incomplete Sections', propName: 'incompleteSections' },
 }
 
 interface NavigationStateI {
