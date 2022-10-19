0c94249f18d54602af41f7f43e4c69647d780638
Ben Pintel <Bpintel@gmail.com>
2022-10-19 13:21:36 -0700

    adds markRecaptureNavButtons to release data entry

--- src/screens/markRecapture/releaseDataEntry/index.tsx
+++ src/screens/markRecapture/releaseDataEntry/index.tsx
@@ -1,13 +1,16 @@
 import { KeyboardAvoidingView, Text, View, VStack } from 'native-base'
 import NavButtons from '../../../components/formContainer/NavButtons'
+import MarkRecaptureNavButtons from '../../../components/markRecapture/MarkRecaptureNavButtons'
 
 export default function ReleaseDataEntry({ navigation }: { navigation: any }) {
   return (
     <>
       <View flex={1} bg='themeGrey'>
-        <VStack space={8} p='10'></VStack>
+        <VStack space={8} p='10'>
+          <Text>TEST RELEASE DATA ENTRY</Text>
+        </VStack>
       </View>
-      <NavButtons navigation={navigation} />
+      <MarkRecaptureNavButtons navigation={navigation} />
     </>
   )
 }
