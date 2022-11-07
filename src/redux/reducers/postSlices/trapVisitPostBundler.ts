8ec1af4b064c00fc08e44d717ff9f6a92d71d04b
Hunter H <hunter.h.dev@gmail.com>
2022-11-07 15:19:16 -0800

    Updates trapVisitPostBundler to handle... connectivity events, and post requests

--- src/redux/reducers/postSlices/trapVisitPostBundler.ts
+++ src/redux/reducers/postSlices/trapVisitPostBundler.ts
@@ -1,46 +1,133 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import api from '../../../api/axiosConfig'
+import { connectionChanged } from '../connectivitySlice'
 
 interface InitialStateI {
   submitted: boolean
+  submissionStatus:
+    | 'not-submitted'
+    | 'submitting...'
+    | 'submission-failed'
+    | 'submission-successful'
   trapVisitSubmissions: TrapVisitSubmissionI[]
+  previousTrapVisitSubmissions: TrapVisitSubmissionI[]
 }
 
 interface TrapVisitSubmissionI {
-programId?: number
-visitTypeId?: number
-trapLocationId?: number
-trapVisitTimeStart?: Date
-trapVisitTimeEnd?: Date
-fishProcessed?: number
-whyFishNotProcessed?: number
-sampleGearId?: number
-coneDepth?: number
-trapInThalweg?: boolean
-trapFunctioning?: number
-whyTrapNotFunctioning?: number
-trapStatusAtEnd?: number
-totalRevolutions?: number
-rpmAtStart?: number
-rpmAtEnd?: number
-inHalfConeConfiguration?: boolean
-debrisVolumeLiters?: number
-qcCompleted?: boolean
-qcCompletedAt?: Date
-comments?: string
-}  
+  id?: number
+  programId?: number
+  visitTypeId?: number
+  trapLocationId?: number
+  trapVisitTimeStart?: Date
+  trapVisitTimeEnd?: Date
+  fishProcessed?: number
+  whyFishNotProcessed?: number
+  sampleGearId?: number
+  coneDepth?: number
+  trapInThalweg?: boolean
+  trapFunctioning?: number
+  whyTrapNotFunctioning?: number
+  trapStatusAtEnd?: number
+  totalRevolutions?: number
+  rpmAtStart?: number
+  rpmAtEnd?: number
+  inHalfConeConfiguration?: boolean
+  debrisVolumeLiters?: number
+  qcCompleted?: boolean
+  qcCompletedAt?: Date
+  comments?: string
+}
+
+interface APIResponseI {
+  data: any
+}
 
 const initialState: InitialStateI = {
   submitted: false,
+  submissionStatus: 'not-submitted',
   trapVisitSubmissions: [],
+  previousTrapVisitSubmissions: [],
 }
+
+// Async actions API calls
+export const postTrapVisitSubmissions = createAsyncThunk(
+  'trapVisitPostBundler/postTrapVisitSubmissions',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState() as InitialStateI
+    const trapVisitSubmissions = state.trapVisitSubmissions
+    const response: APIResponseI = await api.post(
+      'trap-visit/',
+      trapVisitSubmissions
+    )
+    return response.data
+  }
+)
 
 export const trapVisitPostBundler = createSlice({
   name: 'trapVisitPostBundler',
   initialState: initialState,
   reducers: {
     saveTrapVisitSubmission: (state, action) => {
-      // if (state.submitted)
-      state.trapVisitSubmissions.push({ ...action.payload})
+      state.trapVisitSubmissions.push({ ...action.payload })
+      state.submitted = false
+    },
+  },
+  extraReducers: {
+    [postTrapVisitSubmissions.pending.type]: (state, action) => {
+      state.submitted = false
+      state.submissionStatus = 'submitting...'
+    },
+
+    [postTrapVisitSubmissions.fulfilled.type]: (state, action) => {
+      const trapVisitPostResult = action.payload
+      state.submitted = true
+      state.submissionStatus = 'submission-successful'
+      Array.isArray(trapVisitPostResult)
+        ? (state.previousTrapVisitSubmissions = [
+            ...state.previousTrapVisitSubmissions,
+            ...(trapVisitPostResult as TrapVisitSubmissionI[]),
+          ])
+        : (state.previousTrapVisitSubmissions = [
+            ...state.previousTrapVisitSubmissions,
+            trapVisitPostResult as TrapVisitSubmissionI,
+          ])
+      state.trapVisitSubmissions = []
+      console.log('successful post result: ', action.payload)
+    },
+
+    [postTrapVisitSubmissions.rejected.type]: (state, action) => {
+      state.submitted = false
+      state.submissionStatus = 'submission-failed'
+    },
+    [connectionChanged.type]: (state, action) => {
+      if (
+        action.payload.isConnected &&
+        !state.submitted &&
+        state.trapVisitSubmissions.length
+      ) {
+        try {
+          ;(async () => {
+            const trapVisitPostResult: APIResponseI = await api.post(
+              'trap-visit/',
+              state.trapVisitSubmissions
+            )
+            state.submitted = true
+            state.submissionStatus = 'submission-successful'
+            Array.isArray(trapVisitPostResult)
+              ? (state.previousTrapVisitSubmissions = [
+                  ...state.previousTrapVisitSubmissions,
+                  ...trapVisitPostResult as TrapVisitSubmissionI[],
+                ])
+              : (state.previousTrapVisitSubmissions = [
+                  ...state.previousTrapVisitSubmissions,
+                  trapVisitPostResult as TrapVisitSubmissionI,
+                ])
+            state.trapVisitSubmissions = []
+          })()
+        } catch (e) {
+          state.submissionStatus = 'submission-failed'
+        }
+      }
     },
   },
 })
