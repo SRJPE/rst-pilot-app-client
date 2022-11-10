155aa13937dd6d84a58a8e9d6ca7d60ccd780f70
Ben Pintel <Bpintel@gmail.com>
2022-11-10 15:06:05 -0800

    adds QARanges lookup table

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -10,3 +10,17 @@ export const getInitials = (name: string) => {
 
   return result
 }
+
+export const QARanges = {
+  flowMeasure: { max: 3000, min: null },
+  waterTemperature: { max: 100, min: null }, //need F/C
+  waterTurbidity: { max: 1000, min: null },
+  coneDepth: { max: 100, min: null }, //need values
+  totalRevolutions: { max: 25000, min: null },
+  RPM: { max: 30 },
+  forkLength: { max: 1000, min: null }, //need juvenile
+  weight: { max: 400, min: null }, //need juvenile
+  markNumber: { max: 1000, min: null }, //need values
+  plusCount: { max: 1000, min: null }, //need values
+  debrisVolume: { max: 2000, min: null },
+}
