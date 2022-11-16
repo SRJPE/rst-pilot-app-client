fec372ae173145dd48634f5dc60f395b1c145e75
Ben Pintel <Bpintel@gmail.com>
2022-11-16 12:12:26 -0800

    adds historical data slice and saves data to store

--- src/screens/formScreens/HistoricalData.tsx
+++ src/screens/formScreens/HistoricalData.tsx
@@ -12,28 +12,54 @@ import {
 import { useState } from 'react'
 import DateTimePicker from '@react-native-community/datetimepicker'
 import NavButtons from '../../components/formContainer/NavButtons'
+import {
+  markHistoricalDataCompleted,
+  saveHistoricalData,
+} from '../../redux/reducers/formSlices/historicalDataSlice'
+import { connect, useDispatch } from 'react-redux'
+import { AppDispatch, RootState } from '../../redux/store'
 
-export default function HistoricalData({ navigation }: { navigation: any }) {
-  const [startDate, setStartDate] = useState(new Date(1598051730000) as any)
-  const [startTime, setStartTime] = useState(new Date(1598051730000) as any)
-  const [endDate, setEndDate] = useState(new Date(1598051730000) as any)
-  const [endTime, setEndTime] = useState(new Date(1598051730000) as any)
+const mapStateToProps = (state: RootState) => {
+  return {
+    historicalDataStore: state.historicalData.values,
+  }
+}
+
+const HistoricalData = ({
+  navigation,
+  historicalDataStore,
+}: {
+  navigation: any
+  historicalDataStore: any
+}) => {
+  const dispatch = useDispatch<AppDispatch>()
+  const [startDate, setStartDate] = useState(
+    historicalDataStore.startDate as any
+  )
+  const [endDate, setEndDate] = useState(historicalDataStore.endDate as any)
+  const [comments, setComments] = useState(
+    historicalDataStore.comments as string
+  )
 
   const onStartDateChange = (event: any, selectedDate: any) => {
     const currentDate = selectedDate
     setStartDate(currentDate)
   }
-  const onStartTimeChange = (event: any, selectedTime: any) => {
-    const currentTime = selectedTime
-    setStartTime(currentTime)
-  }
+
   const onEndDateChange = (event: any, selectedDate: any) => {
     const currentDate = selectedDate
     setEndDate(currentDate)
   }
-  const onEndTimeChange = (event: any, selectedTime: any) => {
-    const currentTime = selectedTime
-    setEndTime(currentTime)
+
+  const handleSubmit = () => {
+    dispatch(
+      saveHistoricalData({
+        comments,
+        startDate,
+        endDate,
+      })
+    )
+    dispatch(markHistoricalDataCompleted(true))
   }
 
   return (
@@ -41,117 +67,7 @@ export default function HistoricalData({ navigation }: { navigation: any }) {
       <View flex={1} bg='#fff' p='6%' borderColor='themeGrey' borderWidth='15'>
         <VStack space={5}>
           <Heading mb='4'>Select the date and time of the trap visit:</Heading>
-          <Heading color='black' fontSize='xl'>
-            Option 1:
-          </Heading>
-          <HStack space={4} alignItems='center'>
-            <Text color='black' fontSize='xl'>
-              Start Date:
-            </Text>
-            <Input
-              w='1/4'
-              height='50px'
-              fontSize='16'
-              placeholderTextColor='black'
-              placeholder={startDate.toLocaleDateString()}
-              keyboardType='numeric'
-              // onChangeText={handleChange('waterTurbidity')}
-              // onBlur={handleBlur('waterTurbidity')}
-              value={startDate.toLocaleDateString()}
-            ></Input>
-            <Box alignSelf='center' minWidth='130'>
-              <DateTimePicker
-                testID='dateTimePicker'
-                value={startDate}
-                mode='date'
-                is24Hour={true}
-                onChange={onStartDateChange}
-                accentColor='#007C7C'
-              />
-            </Box>
-          </HStack>
-          <HStack space={4} alignItems='center'>
-            <Text color='black' fontSize='xl'>
-              Start Time:
-            </Text>
-            <Input
-              w='1/4'
-              height='50px'
-              fontSize='16'
-              placeholderTextColor='black'
-              placeholder={startTime.toLocaleTimeString([], {
-                timeStyle: 'short',
-              })}
-              keyboardType='numeric'
-            ></Input>
-            <Box alignSelf='center' minWidth='90'>
-              <DateTimePicker
-                testID='dateTimePicker'
-                value={startTime}
-                mode='time'
-                is24Hour={true}
-                onChange={onStartTimeChange}
-                accentColor='#007C7C'
-              />
-            </Box>
-          </HStack>
-          <Divider />
-          <HStack space={4} alignItems='center'>
-            <Text color='black' fontSize='xl'>
-              End Date:
-            </Text>
-            <Input
-              w='1/4'
-              height='50px'
-              fontSize='16'
-              placeholderTextColor='black'
-              placeholder={startDate.toLocaleDateString()}
-              keyboardType='numeric'
-              // onChangeText={handleChange('waterTurbidity')}
-              // onBlur={handleBlur('waterTurbidity')}
-              value={startDate.toLocaleDateString()}
-            ></Input>
-            <Box alignSelf='center' minWidth='130'>
-              <DateTimePicker
-                testID='dateTimePicker'
-                value={endDate}
-                mode='date'
-                is24Hour={true}
-                onChange={onEndDateChange}
-                accentColor='#007C7C'
-              />
-            </Box>
-          </HStack>
-          <HStack space={4} alignItems='center'>
-            <Text color='black' fontSize='xl'>
-              End Time:
-            </Text>
-            <Input
-              w='1/4'
-              height='50px'
-              fontSize='16'
-              placeholderTextColor='black'
-              placeholder={endTime.toLocaleTimeString([], {
-                timeStyle: 'short',
-              })}
-              keyboardType='numeric'
-            ></Input>
-            <Box alignSelf='center' minWidth='90'>
-              <DateTimePicker
-                testID='dateTimePicker'
-                value={endTime}
-                mode='time'
-                is24Hour={true}
-                onChange={onEndTimeChange}
-                accentColor='#007C7C'
-              />
-            </Box>
-          </HStack>
-          <Divider />
 
-          <Heading color='black' fontSize='xl'>
-            Option 2:
-          </Heading>
           <HStack space={4} alignItems='center'>
             <Text color='black' fontSize='xl'>
               Start Date:
@@ -159,10 +75,8 @@ export default function HistoricalData({ navigation }: { navigation: any }) {
 
             <Box alignSelf='center' minWidth='130'>
               <DateTimePicker
-                testID='dateTimePicker'
                 value={startDate}
                 mode='date'
-                is24Hour={true}
                 onChange={onStartDateChange}
                 accentColor='#007C7C'
               />
@@ -170,11 +84,9 @@ export default function HistoricalData({ navigation }: { navigation: any }) {
 
             <Box alignSelf='center' minWidth='90'>
               <DateTimePicker
-                testID='dateTimePicker'
-                value={startTime}
+                value={startDate}
                 mode='time'
-                is24Hour={true}
-                onChange={onStartTimeChange}
+                onChange={onStartDateChange}
                 accentColor='#007C7C'
               />
             </Box>
@@ -185,21 +97,17 @@ export default function HistoricalData({ navigation }: { navigation: any }) {
             </Text>
             <Box alignSelf='center' minWidth='130'>
               <DateTimePicker
-                testID='dateTimePicker'
                 value={endDate}
                 mode='date'
-                is24Hour={true}
                 onChange={onEndDateChange}
                 accentColor='#007C7C'
               />
             </Box>
             <Box alignSelf='center' minWidth='90'>
               <DateTimePicker
-                testID='dateTimePicker'
-                value={endTime}
+                value={endDate}
                 mode='time'
-                is24Hour={true}
-                onChange={onEndTimeChange}
+                onChange={onEndDateChange}
                 accentColor='#007C7C'
               />
             </Box>
@@ -215,14 +123,16 @@ export default function HistoricalData({ navigation }: { navigation: any }) {
               fontSize='16'
               placeholder='Write a comment'
               keyboardType='default'
-              // onChangeText={handleChange('comments')}
-              // onBlur={handleBlur('comments')}
-              // value={values.comments}
+              onChangeText={newText => {
+                setComments(newText)
+              }}
+              value={comments}
             />
           </FormControl>
         </VStack>
       </View>
-      <NavButtons navigation={navigation} />
+      <NavButtons navigation={navigation} handleSubmit={handleSubmit} />
     </>
   )
 }
+export default connect(mapStateToProps)(HistoricalData)
