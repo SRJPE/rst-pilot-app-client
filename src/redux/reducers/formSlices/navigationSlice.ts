dc3ea5630d8981a28f7a06a95ffd891ac2209d5d
Ben Pintel <Bpintel@gmail.com>
2022-11-22 14:46:17 -0600

    removes log

--- src/redux/reducers/formSlices/navigationSlice.ts
+++ src/redux/reducers/formSlices/navigationSlice.ts
@@ -64,7 +64,6 @@ export const navigationSlice = createSlice({
       const stepsArray = Object.values(state.steps).slice(0, 6) as Array<any>
       const incompleteSteps = [] as Array<any>
       for (let step of stepsArray) {
-        console.log('🚀 ~ step completed?', step.name, ':', step.completed)
         if (step.completed === false) {
           incompleteSteps.push(step.completed)
         }
