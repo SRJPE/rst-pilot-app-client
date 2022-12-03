e9628ce0c59070e7c1d0d615c29b50e68322da0e
Ben Pintel <Bpintel@gmail.com>
2022-12-02 16:46:05 -0800

    Adds selected batch characteristics

--- src/screens/formScreens/AddBatch.tsx
+++ src/screens/formScreens/AddBatch.tsx
@@ -1,6 +1,16 @@
 import { Formik } from 'formik'
-import { Box, Center, Divider, Text, View, VStack } from 'native-base'
-import { Keyboard, Pressable } from 'react-native'
+import {
+  Box,
+  Button,
+  Center,
+  Divider,
+  HStack,
+  Text,
+  View,
+  VStack,
+  Pressable,
+} from 'native-base'
+import { Keyboard } from 'react-native'
 import CustomModalHeader from '../../components/Shared/CustomModalHeader'
 
 const AddBatch = ({
@@ -48,11 +58,30 @@ const AddBatch = ({
               />
               <Divider mb='1' />
               <VStack
-                paddingX='10'
+                // paddingX='5'
                 paddingBottom='3'
                 space={3}
-                alignItems='center'
+                // alignItems='center'
               >
+                <Box>
+                  <VStack m='5' space={3}>
+                    <HStack space={4} alignSelf='flex-start'>
+                      <Text fontSize='18' fontWeight='600'>
+                        Selected Batch Characteristics:
+                      </Text>
+                      <Text fontSize='16'>{`Life Stage: ${'fry'}`}</Text>
+                      <Text fontSize='16'>{`Adipose Clipped: ${'Yes'}`}</Text>
+                      <Text fontSize='16'>{`Dead: ${'No'}`}</Text>
+                      <Text fontSize='16'>{`Mark: ${'None'}`}</Text>
+                    </HStack>
+                    <Button bg='primary' w='1/3'>
+                      <Text fontSize='xl' color='white'>
+                        Change Characteristics
+                      </Text>
+                    </Button>
+                  </VStack>
+                  <Divider />
+                </Box>
                 <Box m='5' w='600' h='400' bg='primary'>
                   <Center alignItems='center'>
                     <Text fontSize='20' color='white'>
