2fa908d433ff22d46287294b063a57a7c3364e59
Ben Pintel <Bpintel@gmail.com>
2022-12-26 14:10:53 -0600

    Move functions into parent component

--- src/components/form/FishHoldingCard.tsx
+++ src/components/form/FishHoldingCard.tsx
@@ -1,104 +1,48 @@
 import { Ionicons } from '@expo/vector-icons'
-import { memo, useCallback, useEffect, useState } from 'react'
 import { Badge, Box, VStack, Center, Icon } from 'native-base'
-import { connect, useDispatch, useSelector } from 'react-redux'
-import { AppDispatch, RootState } from '../../redux/store'
 
 const FishHoldingCard = ({
+  cardContent,
   cardTitle,
-  individualFishStore,
+  handlePressRemoveBadge,
 }: {
+  cardContent: Array<any>
   cardTitle: string
-  individualFishStore: Array<any>
+  handlePressRemoveBadge: any
 }) => {
-  const dispatch = useDispatch<AppDispatch>()
-  const dropdownValues = useSelector(
-    (state: RootState) => state.dropdowns.values
-  )
-  const [selectedLifeStages, setSelectedLifeStages] = useState([] as Array<any>)
-  console.log('🚀 ~ FishHoldingCardRendered')
+  console.log('🚀 ~ cardContent', cardContent)
 
-  //update selectedLifeStages when individualFishStore changes
-  useEffect(() => {
-    const lifeStagesStore = []
-    //for each fish in individualFish Array
-    //add to the temp store arr
-    for (let currentFish of individualFishStore) {
-      lifeStagesStore.push(currentFish.lifeStage)
-    }
-    //slickly remove duplicates and set state
-    const lifeStagesSet = [...new Set(lifeStagesStore)]
-    setSelectedLifeStages(lifeStagesSet)
-  }, [individualFishStore])
-
-  const handlePressRemoveBadge = (badgeToRemove: string) => {
-    console.log('🚀 ~ handlePressRemoveBadge ~ badgeToRemove', badgeToRemove)
-
-    //iterate over the selected life stages
-    //find the selected button by its name
-    //remove that item from the list
-    //set the new selectedLifeStages
+  const renderFishHoldingCardContent = () => {
+    return cardContent?.map((item, idx) => {
+      console.log('🚀 ~ cardContent?.map ~ item', item)
+      return (
+        <Badge
+          key={idx}
+          bg='themeOrange'
+          alignSelf='center'
+          justifyContent='space-between'
+          width='70%'
+          variant='solid'
+          shadow='3'
+          _text={{
+            color: 'white',
+            fontSize: '16',
+          }}
+          rightIcon={
+            <Icon
+              as={Ionicons}
+              name={'close'}
+              size='lg'
+              color='white'
+              onPress={() => handlePressRemoveBadge(item, cardTitle)}
+            />
+          }
+        >
+          {item}
+        </Badge>
+      )
+    })
   }
-
-  const renderFishHoldingCardContent = useCallback(() => {
-    return cardTitle === 'Run'
-      ? dropdownValues?.run.map((item, idx) => {
-          return (
-            <Badge
-              key={idx}
-              bg='themeOrange'
-              alignSelf='center'
-              justifyContent='space-between'
-              width='70%'
-              variant='solid'
-              shadow='3'
-              _text={{
-                color: 'white',
-                fontSize: '16',
-              }}
-              rightIcon={
-                <Icon
-                  as={Ionicons}
-                  name={'close'}
-                  size='lg'
-                  color='white'
-                  onPress={() => handlePressRemoveBadge(item.definition)}
-                />
-              }
-            >
-              {item.definition}
-            </Badge>
-          )
-        })
-      : selectedLifeStages.map((item, idx) => {
-          return (
-            <Badge
-              key={idx}
-              bg='themeOrange'
-              alignSelf='center'
-              justifyContent='space-between'
-              width='70%'
-              variant='solid'
-              shadow='3'
-              _text={{
-                color: 'white',
-                fontSize: '16',
-              }}
-              rightIcon={
-                <Icon
-                  as={Ionicons}
-                  name={'close'}
-                  size='lg'
-                  color='white'
-                  onPress={() => handlePressRemoveBadge(item)}
-                />
-              }
-            >
-              {item}
-            </Badge>
-          )
-        })
-  }, [cardTitle, selectedLifeStages])
 
   return (
     <Box w='40%' rounded='xl' overflow='hidden'>
@@ -132,10 +76,4 @@ const FishHoldingCard = ({
   )
 }
 
-const mapStateToProps = (state: RootState) => {
-  return {
-    individualFishStore: state.fishInput.individualFish,
-  }
-}
-
-export default connect(mapStateToProps)(memo(FishHoldingCard))
+export default FishHoldingCard
