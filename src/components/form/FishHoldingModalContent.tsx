14a6a428a3ded04e83ff4c1287f7eef01ff11a01
Ben Pintel <Bpintel@gmail.com>
2022-12-26 14:22:31 -0600

    Moves list manipulation functions... ...into Fish Holding Modal content

--- src/components/form/FishHoldingModalContent.tsx
+++ src/components/form/FishHoldingModalContent.tsx
@@ -1,25 +1,108 @@
 import { HStack, VStack, Text, Button, Divider, Heading } from 'native-base'
-import { memo, useCallback } from 'react'
+import { memo, useCallback, useEffect, useState } from 'react'
+import { connect } from 'react-redux'
+import { RootState } from '../../redux/store'
 import CustomModalHeader from '../Shared/CustomModalHeader'
 import FishHoldingCard from './FishHoldingCard'
 
 const FishHoldingModalContent = ({
+  individualFishStore,
   handleMarkFishFormSubmit,
   closeModal,
 }: {
+  individualFishStore: any
   handleMarkFishFormSubmit?: any
   closeModal: any
 }) => {
-  // console.log('🚀 ~ FishHoldingModalContentRendered')
+  console.log('🚀 ~ individualFishStore', individualFishStore)
+  const [selectedLifeStages, setSelectedLifeStages] = useState([] as Array<any>)
+  const [selectedRuns, setSelectedRuns] = useState([] as Array<any>)
+  const [totalFish, setTotalFish] = useState(0 as number)
 
-  const renderFishHoldingCards = useCallback(() => {
+  useEffect(() => {
+    setSelectedLifeStagesAndRuns()
+    calculateTotalFish()
+  }, [])
+
+  const setSelectedLifeStagesAndRuns = () => {
+    const lifeStagesStore = []
+    const runsStore = []
+    //for each fish in individualFish Array
+    for (let currentFish of individualFishStore) {
+      //do not add yolk sac fry to store
+      if (currentFish.lifeStage === 'yolk sac fry') continue
+      //add `to the temp store arr
+      lifeStagesStore.push(currentFish.lifeStage)
+      runsStore.push(currentFish.run)
+    }
+    //slickly remove duplicates and set state
+    const lifeStagesSet = [...new Set(lifeStagesStore)]
+    const runsSet = [...new Set(runsStore)]
+    setSelectedLifeStages(lifeStagesSet)
+    setSelectedRuns(runsSet)
+  }
+  const calculateTotalFish = () => {
+    //this needs work ask Erin about functionality
+    let count = 0
+    for (let fish of individualFishStore) {
+      //do not add yolk sac fry to count
+      if (fish.lifeStage === 'yolk sac fry') continue
+      if (fish.willBeUsedInRecapture) {
+        count++
+      }
+    }
+    setTotalFish(count)
+  }
+
+  const removeBadgeFromList = (list: Array<any>, badgeToRemove: string) => {
+    for (let i = 0; i < list.length; i++) {
+      //find the selected button by its name
+      if (list[i] === badgeToRemove) {
+        //remove that item from the list
+        list.splice(i, 1)
+      }
+    }
+    return list
+  }
+
+  const handlePressRemoveBadge = (badgeToRemove: string, cardTitle: string) => {
+    console.log('🚀 ~ handlePressRemoveBadge ~ badgeToRemove', badgeToRemove)
+    if (cardTitle === 'Run') {
+      setSelectedRuns([...removeBadgeFromList(selectedRuns, badgeToRemove)])
+    } else {
+      setSelectedLifeStages([
+        ...removeBadgeFromList(selectedLifeStages, badgeToRemove),
+      ])
+    }
+    calculateTotalFish()
+  }
+
+  const handleClearAll = () => {
+    setSelectedRuns([])
+    setSelectedLifeStages([])
+    setTotalFish(0)
+  }
+  const handleResetAll = () => {
+    setSelectedLifeStagesAndRuns()
+    calculateTotalFish()
+  }
+
+  const renderFishHoldingCards = () => {
     return (
       <HStack space={10} justifyContent='center'>
-        <FishHoldingCard cardTitle='Run' />
-        <FishHoldingCard cardTitle='Life Stage' />
+        <FishHoldingCard
+          cardContent={selectedRuns}
+          handlePressRemoveBadge={handlePressRemoveBadge}
+          cardTitle='Run'
+        />
+        <FishHoldingCard
+          cardContent={selectedLifeStages}
+          handlePressRemoveBadge={handlePressRemoveBadge}
+          cardTitle='Life Stage'
+        />
       </HStack>
     )
-  }, [])
+  }
 
   return (
     <>
@@ -65,7 +148,7 @@ const FishHoldingModalContent = ({
               bg='primary'
               alignSelf='flex-start'
               shadow='5'
-              // onPress={handelModalChange}
+              onPress={handleClearAll}
             >
               <Text fontWeight='bold' color='white'>
                 Clear all, I am not holding any fish.
@@ -75,7 +158,7 @@ const FishHoldingModalContent = ({
               bg='primary'
               alignSelf='flex-start'
               shadow='5'
-              // onPress={handelModalChange}
+              onPress={handleResetAll}
             >
               <Text fontWeight='bold' color='white'>
                 Reset All
@@ -83,11 +166,20 @@ const FishHoldingModalContent = ({
             </Button>
           </HStack>
           {renderFishHoldingCards()}
-          <Heading>Total Fish Holding: </Heading>
+          <Heading>
+            <Text>Total Fish Holding:</Text>
+            {totalFish}
+          </Heading>
         </VStack>
       </>
     </>
   )
 }
 
-export default memo(FishHoldingModalContent)
+const mapStateToProps = (state: RootState) => {
+  return {
+    individualFishStore: state.fishInput.individualFish,
+  }
+}
+
+export default connect(mapStateToProps)(FishHoldingModalContent)
