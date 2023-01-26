6caa8433c568f4de20d290abd595084aafba7685
Ben Pintel <Bpintel@gmail.com>
2023-01-26 14:14:54 -0800

    adds graph placeholder

---
+++ src/components/form/batchCount/BatchCountGraph.tsx
@@ -0,0 +1,19 @@
+import { Box, Center, Text } from 'native-base'
+import React from 'react'
+
+const BatchCountGraph = () => {
+  return (
+    <Box
+      h='2/5'
+      w='4/5'
+      alignSelf='center'
+      alignItems='center'
+      justifyContent='center'
+      bg='secondary'
+    >
+      <Text>GRAPH</Text>
+    </Box>
+  )
+}
+
+export default BatchCountGraph
