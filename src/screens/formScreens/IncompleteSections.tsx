c4eb3122d62d5019ac506560cc7e9adc97f8a949
Hunter H <hunter.h.dev@gmail.com>
2023-02-01 16:49:58 -0800

    Save run, run method default, and markedForRelease... to the catch raw submission

--- src/screens/formScreens/IncompleteSections.tsx
+++ src/screens/formScreens/IncompleteSections.tsx
@@ -251,6 +251,7 @@ const IncompleteSections = ({
     const plusCountMethodValues = returnDefinitionArray(
       dropdownsState.values.plusCountMethodology
     )
+    const runValues = returnDefinitionArray(dropdownsState.values.run)
     const returnTaxonCode = (fishSubmissionData: IndividualFishValuesI) => {
       let code = null
       dropdownsState.values.taxon.forEach((taxonValue: any) => {
@@ -274,9 +275,13 @@ const IncompleteSections = ({
         programId: 1,
         trapVisitId: null,
         taxonCode: returnTaxonCode(fishValue),
-        captureRunClass: null,
-        captureRunClassMethod: null,
+        captureRunClass: returnNullableTableId(
+          runValues.indexOf(fishValue.run)
+        ),
+        // save capture run class method as 6 if selected from fish input dropdown
+        captureRunClassMethod: fishValue.run ? 6 : null,
         markType: null, // Check w/ Erin
+        markedForRelease: fishValue.willBeUsedInRecapture,
         adiposeClipped: fishValue.adiposeClipped,
         dead: fishValue.dead,
         lifeStage: returnNullableTableId(
