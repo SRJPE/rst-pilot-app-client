222aa36532ae768c27282426ed8a88efbfdeec13
Hunter H <hunter.h.dev@gmail.com>
2023-02-23 10:42:09 -0800

    Add tabs UI

--- src/navigators/FormStackNavigation.tsx
+++ src/navigators/FormStackNavigation.tsx
@@ -1,6 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import FishInput from '../screens/formScreens/FishInput'
-import { useSelector } from 'react-redux'
+import { connect, useDispatch, useSelector } from 'react-redux'
 import FishProcessing from '../screens/formScreens/FishProcessing'
 import VisitSetup from '../screens/formScreens/VisitSetup'
 import TrapOperations from '../screens/formScreens/TrapOperations'
@@ -17,17 +17,109 @@ import AddFish from '../screens/formScreens/AddFish'
 import PaperEntry from '../screens/formScreens/PaperEntry'
 import StartedTrapping from '../screens/formScreens/StartedTrapping'
 import BatchCount from '../screens/formScreens/BatchCount'
+import {
+  HStack,
+  VStack,
+  Text,
+  Button,
+  Box,
+  IconButton,
+  Icon,
+} from 'native-base'
+import { AppDispatch, RootState } from '../redux/store'
+import {
+  createTab,
+  deleteTab,
+  setActiveTab,
+  TabStateI,
+} from '../redux/reducers/formSlices/tabSlice'
+import { ScrollView } from 'react-native-gesture-handler'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import { uid } from 'uid'
 
 const FormStack = createNativeStackNavigator()
 
-export default function FormStackNavigation() {
+function FormStackNavigation(props: any) {
+  const dispatch = useDispatch<AppDispatch>()
   const fishInputModalOpen = useSelector(
     (state: any) => state.fishInput.modalOpen
   )
+  const tabSlice = props.tabSlice as TabStateI
+
   return (
     <FormStack.Navigator
       initialRouteName='Visit Setup'
-      screenOptions={{ header: (props) => <ProgressHeader {...props} /> }}
+      screenOptions={{
+        header: (props) => (
+          <VStack>
+            <ProgressHeader {...props} />
+            {Object.keys(tabSlice.tabs).length ? (
+              <Box px={'2%'} pt={'2%'}>
+                <ScrollView horizontal={true}>
+                  <HStack alignItems={'center'} justifyContent='space-between'>
+                    {Object.keys(tabSlice.tabs).map((tabId) => (
+                      <Button
+                        bg={
+                          tabId == tabSlice.activeTabID
+                            ? 'primary'
+                            : 'secondary'
+                        }
+                        onPress={() => dispatch(setActiveTab(tabId))}
+                        key={tabId}
+                        mr={5}
+                      >
+                        <HStack
+                          alignItems={'center'}
+                          justifyContent='space-between'
+                        >
+                          <Text
+                            fontSize='lg'
+                            color={
+                              tabId == tabSlice.activeTabID
+                                ? 'white'
+                                : 'primary'
+                            }
+                          >
+                            {tabSlice.tabs[tabId]}
+                          </Text>
+                          <Icon
+                            onPress={() => dispatch(deleteTab(tabId))}
+                            as={Ionicons}
+                            name='ios-close-circle'
+                            color='#FFF'
+                            size={6}
+                            margin='0'
+                            padding='0'
+                            marginLeft={3}
+                          />
+                        </HStack>
+                      </Button>
+                    ))}
+                    <Icon
+                      onPress={() => {
+                        const tabID = uid()
+                        dispatch(createTab({ tabID, tabName: 'New Tab' }))
+                        dispatch(setActiveTab(tabID))
+                        props.navigation.navigate('Trap Visit Form', {
+                          screen: 'Visit Setup',
+                        })
+                      }}
+                      as={Ionicons}
+                      name='ios-add-circle'
+                      color='primary'
+                      size={9}
+                      margin='0'
+                      padding='0'
+                    />
+                  </HStack>
+                </ScrollView>
+              </Box>
+            ) : (
+              <></>
+            )}
+          </VStack>
+        ),
+      }}
     >
       <FormStack.Screen name='Visit Setup' component={VisitSetup} />
       <FormStack.Screen name='Trap Operations' component={TrapOperations} />
@@ -73,3 +165,11 @@ export default function FormStackNavigation() {
     </FormStack.Navigator>
   )
 }
+
+const mapStateToProps = (state: RootState) => {
+  return {
+    tabSlice: state.tabSlice,
+  }
+}
+
+export default connect(mapStateToProps, {})(FormStackNavigation)
