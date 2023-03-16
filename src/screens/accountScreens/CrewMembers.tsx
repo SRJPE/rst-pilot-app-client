45fb20f11356764324a6ccdf8fa5f3e5816d6df3
Ben Pintel <Bpintel@gmail.com>
2023-03-16 13:55:18 -0700

    adds crew members screen

--- src/screens/accountScreens/CrewMembers.tsx
+++ src/screens/accountScreens/CrewMembers.tsx
@@ -1,65 +1,58 @@
 import React from 'react'
 import {
   Box,
-  Button,
-  Center,
-  Divider,
   Heading,
   HStack,
   Icon,
-  IconButton,
   Pressable,
   Text,
-  View,
   VStack,
 } from 'native-base'
-import { Feather, Ionicons } from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons'
+import CreateNewProgramNavButtons from '../../components/createNewProgram/CreateNewProgramNavButtons'
 const CrewMembers = ({ navigation }: { navigation: any }) => {
   const handleEditCrewMember = () => {}
 
   return (
-    <Box overflow='hidden'>
-      <Center
-        bg='primary'
-        _text={{
-          alignSelf: 'center',
-          color: '#FFF',
-          fontWeight: '700',
-          fontSize: 'xl',
-        }}
-        bottom='0'
-        px='6'
-        py='1.5'
-      >
-        Add Trapping Crew
-      </Center>
-      <Button my='5' mx='40' bg='primary' onPress={() => navigation.goBack()}>
-        GO BACK
-      </Button>
-      <VStack py='5%' px='10%' space={5}>
-        <Text fontSize='lg' color='grey'>
-          {
-            'Please add some additional information about yourself and add your crew \nmembers. Accounts will be created for all crew'
-          }
-        </Text>
-        <HStack space={5} alignItems='center'>
-          <Icon as={Ionicons} name='person-circle' size='5xl' color='primary' />
-          <Heading alignSelf='center'>You (Team Lead)</Heading>
-          <Pressable onPress={handleEditCrewMember}>
-            <Ionicons name='md-pencil' size={30} color='grey' />
-          </Pressable>
-        </HStack>
-        <Text>First Name:</Text>
-        <Text>Last Name:</Text>
-        <Text>Email:</Text>
-      </VStack>
-    </Box>
+    <>
+      <Box overflow='hidden' flex={1} bg='#fff'>
+        <Box
+          bg='primary'
+          _text={{
+            color: '#FFF',
+            fontWeight: '700',
+            fontSize: '2xl',
+          }}
+          px='6'
+          py='3'
+        >
+          Permitting Information
+        </Box>
+        <VStack py='5%' px='10%' space={5}>
+          <Text fontSize='lg' color='grey'>
+            {
+              'Please add some additional information about yourself and add your crew \nmembers. Accounts will be created for all crew'
+            }
+          </Text>
+          <HStack space={5} alignItems='center'>
+            <Icon
+              as={Ionicons}
+              name='person-circle'
+              size='5xl'
+              color='primary'
+            />
+            <Heading alignSelf='center'>You (Team Lead)</Heading>
+            <Pressable onPress={handleEditCrewMember}>
+              <Ionicons name='md-pencil' size={30} color='grey' />
+            </Pressable>
+          </HStack>
+          <Text>First Name:</Text>
+          <Text>Last Name:</Text>
+          <Text>Email:</Text>
+        </VStack>
+      </Box>
+      <CreateNewProgramNavButtons navigation={navigation} />
+    </>
   )
 }
 export default CrewMembers
-
-//   {
-//   as: Ionicons,
-//   name: 'md-pencil',
-//   color: 'grey',
-// }
