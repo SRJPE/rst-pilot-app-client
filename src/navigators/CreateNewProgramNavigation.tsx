65b04c6d3f38e12f187d516163fc63bd33b22447
Ben Pintel <Bpintel@gmail.com>
2023-03-16 13:55:44 -0700

    adds more routes to navigation

--- src/navigators/CreateNewProgramNavigation.tsx
+++ src/navigators/CreateNewProgramNavigation.tsx
@@ -1,8 +1,12 @@
 import { createStackNavigator } from '@react-navigation/stack'
 
 import CreateNewProgramHome from '../screens/accountScreens/CreateNewProgramHome'
-import TrappingProtocols from '../screens/accountScreens/TrappingProtocols'
+import TrappingSites from '../screens/accountScreens/TrappingSites'
 import CrewMembers from '../screens/accountScreens/CrewMembers'
+import EfficiencyTrialProtocols from '../screens/accountScreens/EfficiencyTrialProtocols'
+import TrappingProtocols from '../screens/accountScreens/TrappingProtocols'
+import PermitInformation from '../screens/accountScreens/PermitInformation'
+import PermittingInformationInput from '../screens/accountScreens/PermittingInformationInput'
 
 const CreateNewProgram = createStackNavigator()
 
@@ -17,10 +21,26 @@ export default function CreateNewProgramStackNavigator() {
         component={CreateNewProgramHome}
       />
       <CreateNewProgram.Screen
+        name='Trapping Sites'
+        component={TrappingSites}
+      />
+      <CreateNewProgram.Screen name='Crew Members' component={CrewMembers} />
+      <CreateNewProgram.Screen
+        name='Efficiency Trial Protocols'
+        component={EfficiencyTrialProtocols}
+      />
+      <CreateNewProgram.Screen
         name='Trapping Protocols'
         component={TrappingProtocols}
       />
-      <CreateNewProgram.Screen name='Crew Members' component={CrewMembers} />
+      <CreateNewProgram.Screen
+        name='Permit Information'
+        component={PermitInformation}
+      />
+      <CreateNewProgram.Screen
+        name='Permitting Information Input'
+        component={PermittingInformationInput}
+      />
     </CreateNewProgram.Navigator>
   )
 }
