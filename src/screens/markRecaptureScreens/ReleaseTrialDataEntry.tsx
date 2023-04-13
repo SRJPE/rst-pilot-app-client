914422276b3e6e1ba9819c5c4f408e045eba7842
Ben Pintel <Bpintel@gmail.com>
2023-04-13 14:48:06 -0700

    Adds saveMarkRecaptureSubmissions... ...adds markedAt dateTime Picker

--- src/screens/markRecaptureScreens/ReleaseTrialDataEntry.tsx
+++ src/screens/markRecaptureScreens/ReleaseTrialDataEntry.tsx
@@ -1,6 +1,7 @@
 import { Formik } from 'formik'
 import {
   Box,
+  Button,
   Divider,
   FormControl,
   Heading,
@@ -17,7 +18,7 @@ import CustomSelect from '../../components/Shared/CustomSelect'
 import RenderErrorMessage from '../../components/Shared/RenderErrorMessage'
 import { AppDispatch, RootState } from '../../redux/store'
 import Ionicons from '@expo/vector-icons/Ionicons'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import DateTimePicker from '@react-native-community/datetimepicker'
 import { releaseTrialDataEntrySchema } from '../../utils/helpers/yupValidations'
 import {
@@ -28,26 +29,53 @@ import { markActiveMarkRecaptureStepCompleted } from '../../redux/reducers/markR
 import CustomModal from '../../components/Shared/CustomModal'
 import AddAnotherMarkModalContent from '../../components/Shared/AddAnotherMarkModalContent'
 import MarkBadgeList from '../../components/markRecapture/MarkBadgeList'
+import {
+  postMarkRecaptureSubmissions,
+  saveMarkRecaptureSubmission,
+} from '../../redux/reducers/postSlices/markRecapturePostBundler'
+import { ReleaseMarkI } from '../../redux/reducers/addAnotherMarkSlice'
+import { flatten, uniq } from 'lodash'
 
 const mapStateToProps = (state: RootState) => {
   return {
+    releaseTrialState: state.releaseTrial,
     releaseTrialDataEntryState: state.releaseTrialDataEntry,
+    visitSetupState: state.visitSetup,
     visitSetupDefaultsState: state.visitSetupDefaults,
+    connectivityState: state.connectivity,
+    tabState: state.tabSlice,
+    dropdownsState: state.dropdowns,
   }
 }
 
 const ReleaseDataEntry = ({
   navigation,
+  releaseTrialState,
   releaseTrialDataEntryState,
+  visitSetupState,
   visitSetupDefaultsState,
+  connectivityState,
+  tabState,
+  dropdownsState,
 }: {
   navigation: any
+  releaseTrialState: any
   releaseTrialDataEntryState: any
+  visitSetupState: any
   visitSetupDefaultsState: any
+  connectivityState: any
+  tabState: any
+  dropdownsState: any
 }) => {
   const dispatch = useDispatch<AppDispatch>()
   const dropdownValues = useSelector((state: any) => state.dropdowns)
-  const { trapLocations } = visitSetupDefaultsState
+  const tabIds = Object.keys(tabState.tabs)
+  const filteredReleaseSites = visitSetupDefaultsState.releaseSites.filter(
+    (releaseSite: any) =>
+      releaseSite.trapLocationsId ===
+      visitSetupState[tabIds[0]].values.trapLocationId //REFACTOR later
+  )
+  const [markedTime, setMarkedTime] = useState(new Date() as any)
   const [releaseTime, setReleaseTime] = useState(new Date() as any)
   const [addMarkModalOpen, setAddMarkModalOpen] = useState(false as boolean)
 
@@ -55,12 +83,122 @@ const ReleaseDataEntry = ({
     const currentDate = selectedDate
     setReleaseTime(currentDate)
   }
+  const onMarkedTimeChange = (event: any, selectedDate: any) => {
+    const currentDate = selectedDate
+    setMarkedTime(currentDate)
+  }
 
-  const handleSubmit = (values: any) => {
-    dispatch(saveReleaseTrialDataEntry({ ...values, releaseTime: releaseTime }))
-    dispatch(markReleaseTrialDataEntryCompleted(true))
-    dispatch(markActiveMarkRecaptureStepCompleted(true))
+  const returnNullableTableId = (value: any) => (value == -1 ? null : value + 1)
+
+  const returnDefinitionArray = (dropdownsArray: any[]) => {
+    return dropdownsArray.map((dropdownObj: any) => {
+      return dropdownObj.definition
+    })
+  }
+
+  const saveMarkRecaptureSubmissions = () => {
+    const runValues = returnDefinitionArray(dropdownsState.values.run)
+    const markTypeValues = returnDefinitionArray(dropdownsState.values.markType)
+    const markColorValues = returnDefinitionArray(
+      dropdownsState.values.markColor
+    )
+    const bodyPartValues = returnDefinitionArray(dropdownsState.values.bodyPart)
+    const releaseSiteValues = visitSetupDefaultsState.releaseSites.map(
+      (releaseSiteObj: any) => {
+        return releaseSiteObj.releaseSiteName
+      }
+    )
+
+    //crew data lookup string => ID
+    const selectedCrewNamesMap: Record<string, boolean> =
+      releaseTrialDataEntryState.values.crew.reduce(
+        (acc: Record<string, boolean>, name: string) => ({
+          ...acc,
+          [name]: true,
+        }),
+        {}
+      )
+
+    const allCrewObjects = flatten(visitSetupDefaultsState.crewMembers)
+    const selectedCrewIds = uniq(
+      allCrewObjects
+        .filter(
+          (obj: any) => selectedCrewNamesMap[`${obj.firstName} ${obj.lastName}`]
+        )
+        .map((obj: any) => obj.personnelId)
+    )
+
+    const markRecaptureSubmission = {
+      programId: releaseTrialDataEntryState.values.programId,
+      // releasePurposeId: null, //left as null
+      releaseSiteId: returnNullableTableId(
+        releaseSiteValues.indexOf(
+          releaseTrialDataEntryState.values.releaseLocation
+        )
+      ),
+      releasedAt: releaseTrialDataEntryState.values.releaseTime.toLocaleString(
+        'en-US',
+        {
+          //remove when merged with dev
+          timeZone: 'America/Los_Angeles',
+        }
+      ),
+      markedAt: releaseTrialDataEntryState.values.markedTime.toLocaleString(
+        'en-US',
+        {
+          //remove when merged with dev
+          timeZone: 'America/Los_Angeles',
+        }
+      ),
+      marksArray: releaseTrialDataEntryState.values.appliedMarks.map(
+        (markObj: any) => {
+          return {
+            markType: returnNullableTableId(
+              markTypeValues.indexOf(markObj.markType)
+            ),
+            markColor: returnNullableTableId(
+              markColorValues.indexOf(markObj.markColor)
+            ),
+            bodyPart: returnNullableTableId(
+              bodyPartValues.indexOf(markObj.markPosition)
+            ),
+          }
+        }
+      ),
+      runHatcheryFish: returnNullableTableId(
+        runValues.indexOf(releaseTrialState.values.runIDHatchery)
+      ),
+      hatcheryFishWeight: Number(releaseTrialState.values.runWeightHatchery),
+      totalWildFishReleased: Number(releaseTrialState.values.wildCount),
+      totalHatcheryFishReleased: Number(releaseTrialState.values.hatcheryCount),
+      totalWildFishDead: Number(releaseTrialState.values.deadWildCount),
+      totalHatcheryFishDead: Number(releaseTrialState.values.deadHatcheryCount),
+      releaseCrew: selectedCrewIds,
+      markColor: 1, // remove
+      markType: 1, // remove
+      markPosition: 1, // remove
+    }
+
+    dispatch(saveMarkRecaptureSubmission(markRecaptureSubmission))
+    if (connectivityState.isConnected) {
+      console.log('CONNECTED')
+      dispatch(postMarkRecaptureSubmissions())
+    }
+  }
+
+  const handleSubmit = async (values: any) => {
+    await dispatch(
+      saveReleaseTrialDataEntry({
+        ...values,
+        releaseTime: releaseTime,
+        markedTime: markedTime,
+      })
+    )
+    await dispatch(markReleaseTrialDataEntryCompleted(true))
     console.log('🚀 ~ handleSubmit ~ ReleaseTrialDataEntry', values)
+
+    //wait for screen form data to be dispatched to store and then make POST
+    saveMarkRecaptureSubmissions()
   }
 
   return (
@@ -127,15 +265,30 @@ const ReleaseDataEntry = ({
                   placeholder='Location'
                   onValueChange={handleChange('releaseLocation')}
                   setFieldTouched={setFieldTouched}
-                  selectOptions={trapLocations?.map((trapLocation: any) => ({
-                    label: trapLocation?.siteName,
-                    value: trapLocation?.siteName,
-                  }))}
+                  selectOptions={filteredReleaseSites?.map(
+                    (releaseSite: any) => ({
+                      label: releaseSite?.releaseSiteName,
+                      value: releaseSite?.releaseSiteName,
+                    })
+                  )}
                 />
                 {touched.releaseLocation &&
                   errors.releaseLocation &&
                   RenderErrorMessage(errors, 'releaseLocation')}
               </FormControl>
+              <VStack space={2}>
+                <Text color='black' fontSize='xl'>
+                  Confirm Marked Date and Time:
+                </Text>
+                <Box alignSelf='flex-start' minWidth='320' ml='-105'>
+                  <DateTimePicker
+                    value={markedTime}
+                    mode='datetime'
+                    onChange={onMarkedTimeChange}
+                    accentColor='#007C7C'
+                  />
+                </Box>
+              </VStack>
               <VStack space={2}>
                 <Text color='black' fontSize='xl'>
                   Confirm Release Date and Time:
@@ -149,6 +302,9 @@ const ReleaseDataEntry = ({
                   />
                 </Box>
               </VStack>
+              <Button bg='grey' onPress={() => saveMarkRecaptureSubmissions()}>
+                Test save to store
+              </Button>
             </VStack>
           </View>
           <MarkRecaptureNavButtons
