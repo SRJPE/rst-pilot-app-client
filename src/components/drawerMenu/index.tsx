069afb2ebaf41075b4acfa93ab334972c4c08920
Ben Pintel <Bpintel@gmail.com>
2023-04-20 23:32:36 -0700

    adds a dev log button to drawer menu

--- src/components/drawerMenu/index.tsx
+++ src/components/drawerMenu/index.tsx
@@ -151,6 +151,12 @@ const DrawerMenu = (props: DrawerContentComponentProps) => {
             title='Generate Report'
           />
           <MenuButton
+            active={false}
+            onPress={() => console.log('🚀 ~ reduxState:', reduxState)}
+            icon='bug'
+            title='Developer Log'
+          />
+          <MenuButton
             active={currentRoute === 'Mark Recapture'}
             onPress={() => handlePressMainNavButton('Mark Recapture')}
             icon='clipboard'
