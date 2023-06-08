10f795366d885b1318bd79782f8c79ba3ac873be
Hunter H <hunter.h.dev@gmail.com>
2023-06-08 12:28:18 -0700

    Fix failed 'duplicate id' trap visits and... add QC data

--- src/redux/reducers/postSlices/trapVisitFormPostBundler.ts
+++ src/redux/reducers/postSlices/trapVisitFormPostBundler.ts
@@ -2,6 +2,7 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import api from '../../../api/axiosConfig'
 import { RootState } from '../../store'
 import { cloneDeep } from 'lodash'
+import { getSubstring } from '../../../utils/utils'
 
 interface InitialStateI {
   submissionStatus:
@@ -96,66 +97,112 @@ export const postTrapVisitFormSubmissions = createAsyncThunk(
     }
 
     // getting submissions for trap / catch_raw
-    const trapVisitSubmissions =
+    const trapVisitSubmissions: any =
       state.trapVisitFormPostBundler.trapVisitSubmissions
-    const catchRawSubmissions =
+    const catchRawSubmissions: any =
       state.trapVisitFormPostBundler.catchRawSubmissions
 
-    // for each trap visit
-    await Promise.all(
-      trapVisitSubmissions.map(async (trapSubmission: any) => {
-        console.log('hit inside of promise...')
-        const uid = trapSubmission.uid
-        const trapSubmissionCopy = cloneDeep(trapSubmission)
-        delete trapSubmissionCopy['uid']
-        console.log('hit... trapSubmissionCopy: ', trapSubmissionCopy)
-        // submit trap visit
-        const apiResponse: APIResponseI = await api.post(
-          'trap-visit/',
-          trapSubmissionCopy
-        )
-        // get response from server
-        const { createdTrapVisitResponse, createdTrapVisitCrewResponse } =
-          apiResponse.data
-        // save to payload
-        payload.trapVisitResponse.push(createdTrapVisitResponse)
-        // get all linked catch raws for iteration of trap visit
-        const linkedCatchRawSubmissions = catchRawSubmissions.filter(
-          (catchSubmission: any) => catchSubmission.uid === uid
-        )
-        // submit all linked catch_raw's and give id from trap_visit response
-
-        await Promise.all(
-          linkedCatchRawSubmissions.map(async (catchSubmission: any) => {
-            const catchSubmissionCopy = cloneDeep(catchSubmission)
-            delete catchSubmissionCopy['uid']
-            console.log('hit... catchSubmissionCopy: ', catchSubmissionCopy)
-            const apiResponse: APIResponseI = await api.post('catch-raw/', {
-              ...catchSubmissionCopy,
-              trapVisitId: createdTrapVisitResponse.id,
-            })
-
-            const {
-              createdCatchRawResponse,
-              createdGeneticSamplingDataResponse,
-              createdExistingMarksResponse,
-              createdMarkAppliedResponse,
-            } = apiResponse.data
-
-            payload.catchRawResponse.push({
-              createdCatchRawResponse,
-              createdGeneticSamplingDataResponse,
-              createdExistingMarksResponse,
-              createdMarkAppliedResponse,
-            })
+    try {
+      // for each trap visit
+      await Promise.all(
+        trapVisitSubmissions.map(async (trapSubmission: any) => {
+          const uid = trapSubmission.uid
+          const trapSubmissionCopy = cloneDeep(trapSubmission)
+          delete trapSubmissionCopy['uid']
+          // submit trap visit
+          const apiResponse: APIResponseI = await api.post(
+            'trap-visit/',
+            trapSubmissionCopy
+          )
+          // get response from server
+          const {
+            createdTrapVisitResponse,
+            createdTrapVisitCrewResponse,
+            createdTrapCoordinatesResponse,
+            createdTrapVisitEnvironmentalResponse,
+          } = apiResponse.data
+          // save to payload
+          payload.trapVisitResponse.push({
+            createdTrapVisitResponse,
+            createdTrapVisitCrewResponse,
+            createdTrapCoordinatesResponse,
+            createdTrapVisitEnvironmentalResponse,
           })
-        )
+          // get all linked catch raws for iteration of trap visit
+          const linkedCatchRawSubmissions = catchRawSubmissions.filter(
+            (catchSubmission: any) => catchSubmission.uid === uid
+          )
+          // submit all linked catch_raw's and give id from trap_visit response
+
+          await Promise.all(
+            linkedCatchRawSubmissions.map(async (catchSubmission: any) => {
+              const catchSubmissionCopy = cloneDeep(catchSubmission)
+              delete catchSubmissionCopy['uid']
+              const apiResponse: APIResponseI = await api.post('catch-raw/', {
+                ...catchSubmissionCopy,
+                trapVisitId: createdTrapVisitResponse.id,
+              })
+
+              const {
+                createdCatchRawResponse,
+                createdGeneticSamplingDataResponse,
+                createdExistingMarksResponse,
+                createdMarkAppliedResponse,
+              } = apiResponse.data
+
+              payload.catchRawResponse.push({
+                createdCatchRawResponse,
+                createdGeneticSamplingDataResponse,
+                createdExistingMarksResponse,
+                createdMarkAppliedResponse,
+              })
+            })
+          )
+        })
+      )
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue({
+        error: error.response,
+        failedTrapVisitSubmissions: trapVisitSubmissions,
+        failedCatchRawSubmissions: catchRawSubmissions,
       })
-    )
+    }
 
     return payload
   }
 )
+
+const getIndexOfDuplicateTrapVisit = ({
+  errorDetail,
+  failedTrapVisitSubmissions,
+}: {
+  errorDetail: string
+  failedTrapVisitSubmissions: any[]
+}) => {
+  const errorDuplicateValues = getSubstring(
+    errorDetail.slice(58),
+    '(',
+    ')'
+  ).split(',')
+  const programId = Number(errorDuplicateValues[0])
+  const trapLocationId = Number(errorDuplicateValues[1])
+  const trapVisitTimeStart = new Date(errorDuplicateValues[2])
+
+  let index = -1
+  
+  failedTrapVisitSubmissions.forEach((submission, idx) => {
+    const submissionTimeStart = new Date(submission.trapVisitTimeStart)
+    if (
+      submission.programId == programId &&
+      submission.trapLocationId == trapLocationId &&
+      submissionTimeStart.getTime() == trapVisitTimeStart.getTime()
+    ) {
+      index = idx
+    }
+  })
+
+  return index
+}
 
 export const trapVisitPostBundler = createSlice({
   name: 'trapVisitPostBundler',
@@ -196,6 +243,23 @@ export const trapVisitPostBundler = createSlice({
     },
 
     [postTrapVisitFormSubmissions.rejected.type]: (state, action) => {
+      let errorDetail: string = action.payload.error.data.detail
+      let { failedTrapVisitSubmissions, failedCatchRawSubmissions } =
+        action.payload
+      if (errorDetail.includes('already exists')) {
+        if (
+          errorDetail.includes(
+            'Key (program_id, trap_location_id, trap_visit_time_start)'
+          )
+        ) {
+          let index = getIndexOfDuplicateTrapVisit({
+            errorDetail,
+            failedTrapVisitSubmissions,
+          })
+          state.previousTrapVisitSubmissions.push(state.trapVisitSubmissions[index])
+          state.trapVisitSubmissions.splice(index, 1)
+        }
+      }
       state.submissionStatus = 'submission-failed'
     },
   },
