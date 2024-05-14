b7cd6e0f3e80ef8338f97a970758f5bc4587fcaa
Ben Pintel <Bpintel@gmail.com>
2024-05-14 11:35:00 -0700

    adds stack direction prop to FormInputComponent

--- src/components/Shared/FormInputComponent.tsx
+++ src/components/Shared/FormInputComponent.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent } from 'react'
-import { FormControl, HStack, Input, Text } from 'native-base'
+import { FormControl, HStack, Input, Text, VStack } from 'native-base'
 
 import RenderErrorMessage from '../Shared/RenderErrorMessage'
 import {
@@ -16,6 +16,7 @@ interface FormInputComponentI {
   camelName: string
   keyboardType?: KeyboardTypeOptions | undefined
   width?: string
+  stackDirection?: string
   onChangeText: (e: string | ChangeEvent<any>) => void
   onBlur?: (e: NativeSyntheticEvent<TextInputFocusEventData>) => void
 }
@@ -28,6 +29,7 @@ const FormInputComponent: React.FC<FormInputComponentI> = ({
   camelName,
   keyboardType,
   width,
+  stackDirection,
   onChangeText,
   onBlur,
 }) => {
@@ -40,21 +42,56 @@ const FormInputComponent: React.FC<FormInputComponentI> = ({
           </Text>
         </FormControl.Label>
 
-        {touched[camelName] &&
+        {stackDirection !== 'column' &&
+          touched[camelName] &&
           errors[camelName] &&
           RenderErrorMessage(errors, camelName)}
-        {/* <RenderErrorMessage errors={errors} inputName={camelName} /> */}
       </HStack>
-      <Input
-        height='50px'
-        fontSize='16'
-        keyboardType={keyboardType ? keyboardType : 'default'}
-        placeholder={label}
-        onChangeText={onChangeText}
-        onBlur={onBlur}
-        value={value}
-      />
+      <VStack space={2}>
+        <Input
+          height='50px'
+          fontSize='16'
+          keyboardType={keyboardType ? keyboardType : 'default'}
+          placeholder={label}
+          onChangeText={onChangeText}
+          onBlur={onBlur}
+          value={value}
+        />
+        {stackDirection == 'column' &&
+          touched[camelName] &&
+          errors[camelName] &&
+          RenderErrorMessage(errors, camelName)}
+      </VStack>
     </FormControl>
   )
 }
 export default FormInputComponent
+// {
+//   stackDirection === 'column' ? (
+//     <VStack space={4} alignItems='center'>
+//       <FormControl.Label>
+//         <Text color='black' fontSize='xl'>
+//           {label}
+//         </Text>
+//       </FormControl.Label>
+
+//       {touched[camelName] &&
+//         errors[camelName] &&
+//         RenderErrorMessage(errors, camelName)}
+//       {/* <RenderErrorMessage errors={errors} inputName={camelName} /> */}
+//     </VStack>
+//   ) : (
+//     <HStack space={4} alignItems='center'>
+//       <FormControl.Label>
+//         <Text color='black' fontSize='xl'>
+//           {label}
+//         </Text>
+//       </FormControl.Label>
+
+//       {touched[camelName] &&
+//         errors[camelName] &&
+//         RenderErrorMessage(errors, camelName)}
+//       {/* <RenderErrorMessage errors={errors} inputName={camelName} /> */}
+//     </HStack>
+//   )
+// }
