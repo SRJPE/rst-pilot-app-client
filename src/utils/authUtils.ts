d30b7559df5f14d00aa1c0edd13726bd4b039b82
wdwiii <wdwhitfield3@gmail.com>
2024-09-06 18:18:10 -0400

    refactor: Update authUtils.ts to handle token expiration and refresh
    - Add logic to check if the access token has expired and refresh it if necessary
    - Use the refreshAsync function from expo-auth-session to exchange the refresh token for a new access token
    - Update the headers with the new access token and id token
    - Handle different token refresh responses and dispatch actions accordingly
    - Improve logging of token refresh status and network connection status

--- src/utils/authUtils.ts
+++ src/utils/authUtils.ts
@@ -6,7 +6,7 @@ import {
   REACT_APP_CLIENT_ID,
 } from '@env'
 import { error } from 'console'
-import { AppDispatch } from '../redux/store'
+import { AppDispatch, store } from '../redux/store'
 import { setForcedLogoutModalOpen } from '../redux/reducers/userAuthSlice'
 import { access } from 'fs'
 
@@ -15,61 +15,69 @@ type TokenResponse =
   | 'Tokens still valid'
   | 'Tokens could not be refreshed'
   | 'No refresh token found'
+  | 'No network connection, cannot retrieve token'
 
 export const refreshUserToken = async (
   dispatch: AppDispatch
-): Promise<TokenResponse> => {
-  try {
-    const tokenExpiresAt = await SecureStore.getItemAsync(
-      'userAccessTokenExpiresAt'
-    )
-    console.log(
-      '🚀 ~ file: authUtils.ts:26 ~ current token expiration time',
-      moment(tokenExpiresAt).format('MMMM Do YYYY, h:mm:ss a')
-    )
+): Promise<TokenResponse | null> => {
+  const isConnected = store.getState().connectivity.isConnected
 
-    const tokenIsExpired = moment().isAfter(tokenExpiresAt)
+  const tokenExpiresAt = await SecureStore.getItemAsync(
+    'userAccessTokenExpiresAt'
+  )
+  console.log(
+    '🚀 ~ file: authUtils.ts:31 ~ current token expired at: ',
+    moment(tokenExpiresAt).format('MMMM Do YYYY, h:mm:ss a')
+  )
+  if (isConnected) {
+    try {
+      const tokenIsExpired = moment().isAfter(tokenExpiresAt)
 
-    if (tokenIsExpired) {
-      //refreshAsync to exchave for new token
-      const existingRefreshToken = await SecureStore.getItemAsync(
-        'userRefreshToken'
-      )
+      if (tokenIsExpired) {
+        //refreshAsync to exchave for new token
+        const existingRefreshToken = await SecureStore.getItemAsync(
+          'userRefreshToken'
+        )
 
-      const tokenEndpoint =
-        'https://rsttabletapp.b2clogin.com/rsttabletapp.onmicrosoft.com/oauth2/v2.0/token?p=b2c_1_signin'
+        const tokenEndpoint =
+          'https://rsttabletapp.b2clogin.com/rsttabletapp.onmicrosoft.com/oauth2/v2.0/token?p=b2c_1_signin'
 
-      const refreshResponse = await refreshAsync(
-        {
-          clientId: REACT_APP_CLIENT_ID,
-          refreshToken: existingRefreshToken || undefined,
-        },
-        { tokenEndpoint }
-      )
+        const refreshResponse = await refreshAsync(
+          {
+            clientId: REACT_APP_CLIENT_ID,
+            refreshToken: existingRefreshToken || undefined,
+          },
+          { tokenEndpoint }
+        )
 
-      if (refreshResponse.accessToken !== null) {
-        const { accessToken, refreshToken, idToken, issuedAt, expiresIn } =
-          refreshResponse
+        if (refreshResponse.accessToken !== null) {
+          const { accessToken, refreshToken, idToken, issuedAt, expiresIn } =
+            refreshResponse
 
-        await storeAccessTokens({
-          accessToken,
-          refreshToken,
-          idToken,
-          expiresIn,
-          issuedAt,
-        })
-        return 'Tokens refreshed'
-      } else {
-        throw new Error('Tokens could not be refreshed')
+          console.log('🚀 ~ file: authUtils.ts:58 ~ Token exchange successful')
+
+          await storeAccessTokens({
+            accessToken,
+            refreshToken,
+            idToken,
+            expiresIn,
+            issuedAt,
+          })
+          return 'Tokens refreshed'
+        } else {
+          throw new Error('Tokens could not be refreshed')
+        }
+      }
+    } catch (responseError) {
+      if (responseError instanceof Error) {
+        dispatch(setForcedLogoutModalOpen(true))
+        return responseError.message as TokenResponse
       }
     }
-  } catch (responseError) {
-    if (responseError instanceof Error) {
-      dispatch(setForcedLogoutModalOpen(true))
-      return responseError.message as TokenResponse
-    }
+  } else {
+    return 'No network connection, cannot retrieve token' as TokenResponse
   }
-  return 'Tokens still valid' as TokenResponse
+  return null
 }
 
 export const storeAccessTokens = async ({
@@ -91,11 +99,11 @@ export const storeAccessTokens = async ({
 
   // Setting the token expiration to 10 seconds for testing purposes
   const formattedTokenExpiresAt = moment(
-    (10 as number) * 1000 + issuedAt * 1000
+    (30 as number) * 1000 + issuedAt * 1000
   ).format()
 
   console.log(
-    '🚀 ~ file: authUtils.ts:104 ~ New token expiration time',
+    '🚀 ~ file: authUtils.ts:107 ~ New token expiration time',
     moment(formattedTokenExpiresAt).format('MMMM Do YYYY, h:mm:ss a')
   )
   await SecureStore.setItemAsync('userAccessToken', accessToken)
