c8135fe9ebc3dc65cc4bf710dc0b460f89e86a6b
wdwiii <wdwhitfield3@gmail.com>
2025-01-07 23:43:26 -0500

    Refactor HatcheryInformation component for improved layout and usability

--- src/screens/accountScreens/createNewProgram/HatcheryInformation.tsx
+++ src/screens/accountScreens/createNewProgram/HatcheryInformation.tsx
@@ -104,10 +104,11 @@ const HatcheryInformation = ({
                   <Heading alignSelf='center'>Hatchery Information</Heading>
                   <HStack space={10}>
                     <VStack space={2}>
-                      <Text color='black' fontSize='xl'>
+                      <Text color='black' fontSize='md'>
                         Agreement Start Date
                       </Text>
-                      <Box alignSelf='flex-start' minWidth='220' ml='-95'>
+
+                      <Box alignSelf='flex-start'>
                         <DateTimePicker
                           value={agreementStartDate}
                           mode='date'
@@ -117,10 +118,11 @@ const HatcheryInformation = ({
                       </Box>
                     </VStack>
                     <VStack space={2}>
-                      <Text color='black' fontSize='xl'>
+                      <Text color='black' fontSize='md'>
                         Agreement Start Date
                       </Text>
-                      <Box alignSelf='flex-start' minWidth='220' ml='-95'>
+
+                      <Box alignSelf='flex-start'>
                         <DateTimePicker
                           value={agreementEndDate}
                           mode='date'
@@ -130,10 +132,10 @@ const HatcheryInformation = ({
                       </Box>
                     </VStack>
                     <VStack space={2}>
-                      <Text color='black' fontSize='xl'>
+                      <Text color='black' fontSize='md'>
                         Agreement Renewal Date
                       </Text>
-                      <Box alignSelf='flex-start' minWidth='220' ml='-95'>
+                      <Box alignSelf='flex-start'>
                         <DateTimePicker
                           value={renewalDate}
                           mode='date'
@@ -146,6 +148,7 @@ const HatcheryInformation = ({
                   <VStack space={4}>
                     <FormInputComponent
                       label={'Hatchery'}
+                      placeholder='Enter Hatchery Name'
                       touched={touched}
                       errors={errors}
                       value={values.hatchery ? `${values.hatchery}` : ''}
@@ -154,29 +157,25 @@ const HatcheryInformation = ({
                       onBlur={handleBlur('hatchery')}
                     />
                     <HStack space={10} alignItems='center'>
-                      <FormControl width={'45%'}>
-                        <FormControl.Label>
-                          <Text color='black' fontSize='xl'>
-                            Frequency of receiving fish{' '}
-                          </Text>
-                        </FormControl.Label>
-                        <CustomSelect
-                          selectedValue={values.frequencyOfReceivingFish}
-                          placeholder={'Frequency'}
-                          onValueChange={(value: any) =>
-                            handleChange('frequencyOfReceivingFish')(value)
-                          }
-                          setFieldTouched={() =>
-                            setFieldTouched('frequencyOfReceivingFish')
-                          }
-                          selectOptions={dropdownValues?.frequency}
-                        />
-                      </FormControl>
-                      <FormInputComponent
-                        width={'45%'}
-                        label={
-                          'Expected number of fish received at each pickup'
+                      <CustomSelect
+                        label='Frequency of Receiving Fish'
+                        selectedValue={values.frequencyOfReceivingFish}
+                        placeholder={'Select Frequency'}
+                        camelName='frequencyOfReceivingFish'
+                        touched={touched}
+                        errors={errors}
+                        onValueChange={(value: any) =>
+                          handleChange('frequencyOfReceivingFish')(value)
                         }
+                        setFieldTouched={() =>
+                          setFieldTouched('frequencyOfReceivingFish')
+                        }
+                        selectOptions={dropdownValues?.frequency}
+                      />
+
+                      <FormInputComponent
+                        label={'Expected # of Fish Received at Pickup'}
+                        placeholder='0'
                         touched={touched}
                         errors={errors}
                         value={
