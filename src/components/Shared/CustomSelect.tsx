350bff662f8bfbbbe8b55326703654b7f88ef080
wdwiii <wdwhitfield3@gmail.com>
2025-01-10 10:59:44 -0500

    Refactor CustomSelect component for improved layout and error handling

--- src/components/Shared/CustomSelect.tsx
+++ src/components/Shared/CustomSelect.tsx
@@ -47,82 +47,84 @@ const CustomSelect: React.FC<CustomSelectI> = ({
   const showError = hasError && isTouched
 
   return (
-    <FormControl flex={1}>
-      <FormControl.Label>
-        <Text color={showError ? 'red.700' : 'black'} fontSize='md'>
-          {label}
-        </Text>
-      </FormControl.Label>
-      <Select
-        borderColor={showError ? 'red.700' : 'muted.300'}
-        height='50px'
-        fontSize='16'
-        selectedValue={selectedValue ?? ''}
-        minWidth='100'
-        style={style}
-        accessibilityLabel={placeholder}
-        placeholder={placeholder}
-        _selectedItem={{
-          bg: 'secondary',
-          endIcon: <CheckIcon size='6' />,
-        }}
-        mt={1}
-        onValueChange={handleOnChange}
-        onClose={() => {
-          if (setFieldTouched) setFieldTouched()
-        }}
-        isDisabled={disabled}
-      >
-        {selectOptions ? (
-          selectOptions.map((item, idx) => {
-            if (dataType === 'fundingAgency') {
-              return (
-                <Select.Item
-                  key={item.id ?? idx}
-                  label={item.definition}
-                  value={item.definition}
-                />
-              )
-            } else if (item.value) {
-              return (
-                <Select.Item
-                  key={item.id ?? idx}
-                  label={
-                    placeholder === 'Species'
-                      ? item.label
-                      : item.label.replace(/\w+/g, capitalize)
-                  }
-                  value={item.value}
-                />
-              )
-            } else if (item.definition) {
-              return (
-                <Select.Item
-                  key={item.id}
-                  label={
-                    placeholder === 'Species'
-                      ? item.definition
-                      : item.definition.replace(/\w+/g, capitalize)
-                  }
-                  value={item.definition}
-                />
-              )
-            }
-          })
-        ) : (
-          <Select.Item
-            key={'not received from api'}
-            label={'No options found... connect to wifi!'}
-            value={'No options found... connect to wifi!'}
-          />
-        )}
-      </Select>
-      <Box mt={2} h={25}>
-        {showError && (
-          <RenderErrorMessage errors={errors} inputName={camelName} />
-        )}
-      </Box>
-    </FormControl>
+    <Box minH={100}>
+      <FormControl flex={1}>
+        <FormControl.Label>
+          <Text color={showError ? 'red.700' : 'black'} fontSize='md'>
+            {label}
+          </Text>
+        </FormControl.Label>
+        <Select
+          borderColor={showError ? 'red.700' : 'muted.300'}
+          height='50px'
+          fontSize='16'
+          selectedValue={selectedValue ?? ''}
+          minWidth='100'
+          style={style}
+          accessibilityLabel={placeholder}
+          placeholder={placeholder}
+          _selectedItem={{
+            bg: 'secondary',
+            endIcon: <CheckIcon size='6' />,
+          }}
+          mt={1}
+          onValueChange={handleOnChange}
+          onClose={() => {
+            if (setFieldTouched) setFieldTouched()
+          }}
+          isDisabled={disabled}
+        >
+          {selectOptions ? (
+            selectOptions.map((item, idx) => {
+              if (dataType === 'fundingAgency') {
+                return (
+                  <Select.Item
+                    key={item.id ?? idx}
+                    label={item.definition}
+                    value={item.definition}
+                  />
+                )
+              } else if (item.value) {
+                return (
+                  <Select.Item
+                    key={item.id ?? idx}
+                    label={
+                      placeholder === 'Species'
+                        ? item.label
+                        : item.label.replace(/\w+/g, capitalize)
+                    }
+                    value={item.value}
+                  />
+                )
+              } else if (item.definition) {
+                return (
+                  <Select.Item
+                    key={item.id}
+                    label={
+                      placeholder === 'Species'
+                        ? item.definition
+                        : item.definition.replace(/\w+/g, capitalize)
+                    }
+                    value={item.definition}
+                  />
+                )
+              }
+            })
+          ) : (
+            <Select.Item
+              key={'not received from api'}
+              label={'No options found... connect to wifi!'}
+              value={'No options found... connect to wifi!'}
+            />
+          )}
+        </Select>
+        <Box mt={2} h={25}>
+          {showError && (
+            <RenderErrorMessage errors={errors} inputName={camelName} />
+          )}
+        </Box>
+      </FormControl>
+    </Box>
   )
 }
 export default memo(CustomSelect)
