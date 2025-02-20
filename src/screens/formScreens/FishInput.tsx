084efe8728018df3eac67cb74035734fd86a5bbb
wdwiii <wdwhitfield3@gmail.com>
2025-02-20 14:15:16 -0500

    adds fish input validation and error handling in TabBar and FishInput components
    Co-authored-by: Jordan Hoang <jordan.n.hoang@gmail.com>

--- src/screens/formScreens/FishInput.tsx
+++ src/screens/formScreens/FishInput.tsx
@@ -81,6 +81,8 @@ const FishInput = ({
       ? ([...speciesCaptured] as Array<string>)
       : (['YOY Chinook'] as Array<string>)
   )
+  const errorMessage =
+    tabSlice.tabs[activeTabId]?.errorDetails['Fish Input']?.fishStore
 
   useEffect(() => {
     checkboxGroupValue.length < 1 ? setShowError(true) : setShowError(false)
@@ -188,6 +190,11 @@ const FishInput = ({
                 Add Plus Counts
               </Text>
             </Button>
+          </HStack>
+          <HStack>
+            <Text style={{ color: 'red', paddingHorizontal: 20 }}>
+              {errorMessage}
+            </Text>
           </HStack>
 
           <Box px='4'>
