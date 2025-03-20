25f07b3eaa888e289219c2dfa3bf590e38ead39f
Jordan Hoang <jordan.n.hoang@gmail.com>
2025-03-20 11:51:02 -0700

    adds scrollview to incomplete

--- src/screens/formScreens/IncompleteSections.tsx
+++ src/screens/formScreens/IncompleteSections.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { Heading, View, VStack } from 'native-base'
+import { Heading, ScrollView, View, VStack } from 'native-base'
 import { connect, useDispatch } from 'react-redux'
 import { AppDispatch, RootState } from '../../redux/store'
 import navigationSlice, {
@@ -568,7 +568,7 @@ const IncompleteSections = ({
 
   return (
     <>
-      <View
+      <ScrollView
         flex={1}
         bg='#fff'
         // justifyContent='center'
@@ -593,7 +593,7 @@ const IncompleteSections = ({
             )
           })}
         </VStack>
-      </View>
+      </ScrollView>
       <NavButtons
         navigation={navigation}
         handleSubmit={emitSubmission}
