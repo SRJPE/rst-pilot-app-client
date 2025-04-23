31fe62a9a4d3b38a1e9a55e40ab99aac181ed57f
wdwiii <wdwhitfield3@gmail.com>
2025-04-22 23:30:04 -0400

    feat: update water turbidity input handling and validation logic

--- src/utils/helpers/yupValidations.ts
+++ src/utils/helpers/yupValidations.ts
@@ -37,11 +37,15 @@ export const trapOperationsSchema = yup.object().shape({
 
   flowMeasureUnit: yup.string(),
   waterTemperatureUnit: yup.string(),
+  // waterTurbidity: yup
+  //   .number()
+  //   .nullable()
+  //   .typeError('Value must be a number'),
   waterTurbidity: yup
-    .number()
-    .nullable()
-    // .required('Water Turbidity Required')
-    .typeError('Value must be a number'),
+    .mixed()
+    .test('is-empty-or-number', 'Value must be a number', value => {
+      return value.trim() === '' || value === null || !isNaN(value)
+    }),
   waterTurbidityUnit: yup.string(),
   rpm1: yup
     .number()
@@ -72,15 +76,15 @@ export const trapPostProcessingSchema = yup.object().shape({
     .required('Debris volume required'),
   totalRevolutions: yup.number().nullable().typeError('Value must be a number'),
   // .required('Total revolutions required'),
-  isWaterTurbidityPresent: yup.boolean(),
-  waterTurbidity: yup.number().when('isWaterTurbidityPresent', {
-    is: true,
-    then: yup
-      .number()
-      .typeError('Value must be a number')
-      .required('Water Turbidity Required'),
-    otherwise: yup.number().nullable(),
-  }),
+  // waterTurbidity: yup
+  //   .number()
+  //   .typeError('Value must be a number')
+  //   .required('Water Turbidity Required'),
+  waterTurbidity: yup
+    .mixed()
+    .test('is-empty-or-number', 'Value must be a number', value => {
+      return value.trim() === '' || value === null || !isNaN(value)
+    }),
   comments: yup.string(),
   rpm1: yup
     .number()
