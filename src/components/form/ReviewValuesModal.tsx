94841dcb14bc9b9f834d326cff947df5b9c282c1
Jordan Hoang <jordan.n.hoang@gmail.com>
2025-05-13 12:03:03 -0500

    implements fish input review section

--- src/components/form/ReviewValuesModal.tsx
+++ src/components/form/ReviewValuesModal.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { SafeAreaView, Dimensions } from 'react-native'
 import CustomModal from '../Shared/CustomModal'
 import CustomModalHeader from '../Shared/CustomModalHeader'
-import { Text, Button, ScrollView, Divider } from 'native-base'
+import { Text, Button, ScrollView, Divider, HStack, View } from 'native-base'
 import { List } from 'react-native-paper'
 import { SceneMap, TabBar, TabBarProps, TabView } from 'react-native-tab-view'
 import { startCase } from 'lodash'
@@ -15,18 +15,66 @@ type Props = {
   isOpen: boolean
   tabState: any
 }
+const hiddenFields = ['isWaterTurbidityPresent']
+
+function groupBySpeciesForkLength(data: Array<any>) {
+  const result = {} as any
+  let totalCount = 0 as number
+
+  Object.values(data).forEach((fish: any) => {
+    const { species, forkLength, numFishCaught, run } = fish
+
+    totalCount += Number(numFishCaught)
+
+    if (fish.plusCount) {
+      if (!result[`${species} - ${run} Plus Count`]) {
+        result[`${species} - ${run} Plus Count`] = Number(numFishCaught)
+      } else {
+        result[`${species} - ${run} Plus Count`] += Number(numFishCaught)
+      }
+      return
+    }
+
+    if (!result[species]) {
+      result[species] = []
+    }
+
+    // Add `forkLength` repeated `numFishCaught` times
+    for (let i = 0; i < numFishCaught; i++) {
+      if (forkLength) {
+        result[species].push(forkLength)
+      }
+    }
+  })
+
+  // Sort the result object by its keys alphabetically
+  const sortedResult = Object.keys(result)
+    .sort()
+    .reduce((acc, key) => {
+      acc[key] = result[key]
+      return acc
+    }, {} as any)
+
+  Object.assign(result, sortedResult)
+
+  sortedResult['totalCount'] = totalCount
+
+  return sortedResult
+}
 
 const AccordionListItem = ({
   field,
   sectionValues,
+  sectionTitle,
 }: {
   field: string
   sectionValues: any
+  sectionTitle: string
 }) => {
   // if unit field, do not show
   if (field.includes('Unit')) return null
+  if (hiddenFields.includes(field)) return null
 
-  console.log('sectionValues', sectionValues)
   let fieldName = field
   // if field has a number in it, capitalize
   if (/\d/.test(field)) {
@@ -41,43 +89,62 @@ const AccordionListItem = ({
     fieldValue = fieldValue.toLocaleString()
   } else if (typeof fieldValue === 'boolean') {
     fieldValue = fieldValue ? 'Yes' : 'No'
+  } else if (Array.isArray(sectionValues[field])) {
+    if (sectionValues[field].length) {
+      fieldValue = fieldValue.join(', ')
+      if (sectionTitle === 'Fish Input') {
+        fieldValue = `${fieldValue} (Count: ${sectionValues[field].length})`
+      }
+    }
   }
 
   return field && fieldValue ? (
     <List.Item
       title={fieldName}
-      titleStyle={{ fontSize: 16 }}
+      titleStyle={{ fontSize: 16, fontWeight: 'bold' }}
+      style={{ borderBottomColor: 'gray', borderBottomWidth: 1 }}
       right={() => (
-        <Text fontSize={'md'}>
-          {fieldValue} {sectionValues[`${field}Unit`] || ''}
-        </Text>
+        <View style={{ width: '50%', alignItems: 'flex-end' }}>
+          <Text fontSize={'md'}>
+            {fieldValue} {sectionValues[`${field}Unit`] || ''}
+          </Text>
+        </View>
       )}
     />
   ) : null
 }
 
 const AccordionView = ({ tabValues }: { tabValues?: any }) => {
+  const filteredTrapOperationsState = {
+    ...tabValues.trapOperationsState,
+  }
+  delete filteredTrapOperationsState.trapVisitStartTime
+
+  const fishInputLookupObj = groupBySpeciesForkLength(tabValues.fishInputState)
   return (
     <ScrollView>
       <List.Accordion title='Visit Setup' titleStyle={{ fontSize: 20 }}>
         <AccordionListItem
           field={'crew'}
           sectionValues={tabValues.visitSetupState}
+          sectionTitle='Visit Setup'
         />
         {'dataRecorder' in tabValues.visitSetupState && (
           <AccordionListItem
             field={'dataRecorder'}
             sectionValues={tabValues.visitSetupState}
+            sectionTitle='Visit Setup'
           />
         )}
         <Divider width={'97%'} alignSelf={'center'} />
       </List.Accordion>
       <List.Accordion title='Trap Operations' titleStyle={{ fontSize: 20 }}>
-        {Object.keys(tabValues.trapOperationsState).map(key => {
+        {Object.keys(filteredTrapOperationsState).map(key => {
           return (
             <AccordionListItem
               field={key}
-              sectionValues={tabValues.trapOperationsState}
+              sectionValues={filteredTrapOperationsState}
+              sectionTitle='Trap Operations'
             />
           )
         })}
@@ -88,20 +155,35 @@ const AccordionView = ({ tabValues }: { tabValues?: any }) => {
             <AccordionListItem
               field={key}
               sectionValues={tabValues.fishProcessingState}
+              sectionTitle='Fish Processing'
             />
           )
         })}
       </List.Accordion>
       <List.Accordion title='Fish Input' titleStyle={{ fontSize: 20 }}>
-        <List.Item title='First item' />
-        <List.Item title='Second item' />
+        {Object.keys(fishInputLookupObj).map(key => {
+          return (
+            <AccordionListItem
+              field={key}
+              sectionValues={fishInputLookupObj}
+              sectionTitle='Fish Input'
+            />
+          )
+        })}
       </List.Accordion>
       <List.Accordion
         title='Trap Post-Processing'
         titleStyle={{ fontSize: 20 }}
       >
-        <List.Item title='First item' />
-        <List.Item title='Second item' />
+        {Object.keys(tabValues.trapPostProcessingState).map(key => {
+          return (
+            <AccordionListItem
+              field={key}
+              sectionValues={tabValues.trapPostProcessingState}
+              sectionTitle='Trap Post-Processing'
+            />
+          )
+        })}
       </List.Accordion>
     </ScrollView>
   )
@@ -113,8 +195,6 @@ const ReviewValuesModal = ({
   isOpen,
   tabState,
 }: Props) => {
-  console.log('formValues', formValues)
-  console.log('tabState', tabState)
   const [index, setIndex] = useState(0)
   const [routes, setRoutes] = useState(
     [] as Array<{ key: string; title: string }>
@@ -127,8 +207,6 @@ const ReviewValuesModal = ({
       title: tabState.tabs[tabId].name,
     }))
     setRoutes(tabRoutes)
-
-    console.log('tabRoutes', tabRoutes)
 
     const tabScenesMapObj = {} as any
     tabRoutes.forEach((tab: any) => {
@@ -185,18 +263,31 @@ const ReviewValuesModal = ({
             initialLayout={initialLayout}
             commonOptions={{ labelStyle: { color: '#007C7C' } }}
           />
-
-          <Button
-            my={5}
-            mx='auto'
-            minWidth={300}
-            bgColor='primary'
-            onPress={handleCloseReviewValuesModal}
-          >
-            <Text fontSize='xl' color='white'>
-              Close
-            </Text>
-          </Button>
+          <HStack>
+            <Button
+              my={5}
+              mx='auto'
+              minWidth={300}
+              bgColor='gray.400'
+              onPress={handleCloseReviewValuesModal}
+            >
+              <Text fontSize='xl' color='white'>
+                Close
+              </Text>
+            </Button>
+            <Button
+              my={5}
+              mx='auto'
+              minWidth={300}
+              bgColor='primary'
+              colorScheme='coolGray'
+              onPress={handleCloseReviewValuesModal}
+            >
+              <Text fontSize='xl' color='white'>
+                Export as File
+              </Text>
+            </Button>
+          </HStack>
         </>
       </CustomModal>
     </SafeAreaView>
