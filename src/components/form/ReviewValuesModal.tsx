acee6da402b8443a9e9ec09c6bcaac317d1ceb78
Jordan Hoang <jordan.n.hoang@gmail.com>
2025-05-15 12:33:30 -0500

    refactors review post processing

--- src/components/form/ReviewValuesModal.tsx
+++ src/components/form/ReviewValuesModal.tsx
@@ -38,6 +38,14 @@ const getFilteredTrapOperationsState = (trapOperationsState: any) => {
   return filteredTrapOperationsState
 }
 
+const getFilteredPostProcessingState = (trapPostProcessingState: any) => {
+  let filteredTrapPostProcessingState = {
+    ...trapPostProcessingState,
+  }
+  delete filteredTrapPostProcessingState.fishProcessedResult
+  return filteredTrapPostProcessingState
+}
+
 const getProgramFormFieldsLookup = (
   visitSetupState: any,
   visitSetupDefaultState: any
@@ -153,8 +161,9 @@ export function generateAccordionHtmlFromTabValues({
           formValues?.fishProcessingState?.[route.key]?.values
         const fishInputState =
           formValues?.fishInputState?.[route.key]?.fishStore
-        const trapPostProcessingState =
+        const trapPostProcessingState = getFilteredPostProcessingState(
           formValues?.trapPostProcessingState?.[route.key]?.values
+        )
         // const filteredTrapOps = { ...trapOperationsState }
 
         const fishInputLookupObj = fishInputState
@@ -269,6 +278,10 @@ const AccordionView = ({
   const filteredTrapOperationsState = getFilteredTrapOperationsState(
     tabValues.trapOperationsState
   )
+
+  const filteredTrapPostProcessingState = getFilteredPostProcessingState(
+    tabValues.trapPostProcessingState
+  )
   const fishInputLookupObj = tabValues.fishInputState
     ? groupBySpeciesForkLength(tabValues.fishInputState)
     : {}
@@ -354,12 +367,12 @@ const AccordionView = ({
         title='Trap Post-Processing'
         titleStyle={{ fontSize: 20 }}
       >
-        {tabValues.trapPostProcessingState ? (
-          Object.keys(tabValues.trapPostProcessingState).map(key => {
+        {filteredTrapPostProcessingState ? (
+          Object.keys(filteredTrapPostProcessingState).map(key => {
             return (
               <AccordionListItem
                 field={key}
-                sectionValues={tabValues.trapPostProcessingState}
+                sectionValues={filteredTrapPostProcessingState}
                 sectionTitle='Trap Post-Processing'
                 programFormFieldsObj={programFormFieldsObj}
               />
