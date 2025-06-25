46d891c29376f38569454aa0a21434e4452f7113
Jordan Hoang <jordan.n.hoang@gmail.com>
2025-06-25 10:03:05 -0500

    refactors fish input table obj creation. adds location id fallback

--- src/components/form/FishInputDataTable.tsx
+++ src/components/form/FishInputDataTable.tsx
@@ -108,16 +108,10 @@ const FishInputDataTable = ({
 
     const keys = Object.keys(obj)
     keys.forEach(key => {
-      let dataObj: any = cloneDeep(obj[Number(key)])
-      dataObj.existingMarks = dataObj.existingMarks.length
-      delete dataObj.UID
-      delete dataObj.taxonCode
-      delete dataObj.fishConditions
-      delete dataObj.comments
-      delete dataObj.appliedMarks
-      delete dataObj.geneticSamples
-      delete dataObj.captureRunClassMethod
-      delete dataObj.fishObjId
+      const dataObj = pick(
+        cloneDeep(obj[Number(key)]),
+        sortedDataByHeaders
+      ) as any
       let dataObjPadded = { ...emptyTableData, ...dataObj }
 
       const dataObjKeys = Object.keys(dataObjPadded)
